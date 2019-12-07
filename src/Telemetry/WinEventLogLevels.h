03526831b48a451d8aa1d32c5f0f7a34034710c3
Manu Sivasankara Pillai <mapill@microsoft.com>
2019-12-06 20:14:02 -0800

    Adding Telemetry shared project to the PackageManager client project

---
+++ src/Telemetry/WinEventLogLevels.h
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+// From winmeta.h
+
+#define WINEVENT_LEVEL_LOG_ALWAYS 0x0
+#define WINEVENT_LEVEL_CRITICAL 0x1
+#define WINEVENT_LEVEL_ERROR 0x2
+#define WINEVENT_LEVEL_WARNING 0x3
+#define WINEVENT_LEVEL_INFO 0x4
+#define WINEVENT_LEVEL_VERBOSE 0x5
