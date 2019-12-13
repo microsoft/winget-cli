dc5ef5c3e4fb4d9715c74d878c8ca5a6071e68c2
Manu Sivasankara Pillai <mapill@microsoft.com>
2019-12-13 10:43:49 -0800

    Untabyfying

--- src/Telemetry/TelemetryWrapper.cpp
+++ src/Telemetry/TelemetryWrapper.cpp
@@ -8,33 +8,33 @@
 
 namespace AppInstaller::CLI
 {
-	TelemetryTraceLogger::TelemetryTraceLogger()
-	{
-		RegisterTraceLogging();
-	}
+    TelemetryTraceLogger::TelemetryTraceLogger()
+    {
+        RegisterTraceLogging();
+    }
 
-	TelemetryTraceLogger::~TelemetryTraceLogger()
-	{
-		UnRegisterTraceLogging();
-	}
+    TelemetryTraceLogger::~TelemetryTraceLogger()
+    {
+        UnRegisterTraceLogging();
+    }
 
-	TelemetryTraceLogger& TelemetryTraceLogger::GetInstance()
-	{
-		static TelemetryTraceLogger instance;
-		return instance;
-	}
+    TelemetryTraceLogger& TelemetryTraceLogger::GetInstance()
+    {
+        static TelemetryTraceLogger instance;
+        return instance;
+    }
 
-	void TelemetryTraceLogger::LogMessage(std::wstring_view message)
-	{
-		if (!g_IsTelemetryProviderEnabled)
-		{
-			TraceLoggingWrite(g_hTelemetryProvider,
-				PKGMGR_CLIENT_EVENT_INFO,
-				TraceLoggingWideString(message.data(), PKGMGR_CLIENT_MESSAGE),
-				TelemetryPrivacyDataTag(PDT_ProductAndServicePerformance),
-				TraceLoggingKeyword(MICROSOFT_KEYWORD_MEASURES));
-		}
+    void TelemetryTraceLogger::LogMessage(std::wstring_view message)
+    {
+        if (!g_IsTelemetryProviderEnabled)
+        {
+            TraceLoggingWrite(g_hTelemetryProvider,
+                PKGMGR_CLIENT_EVENT_INFO,
+                TraceLoggingWideString(message.data(), PKGMGR_CLIENT_MESSAGE),
+                TelemetryPrivacyDataTag(PDT_ProductAndServicePerformance),
+                TraceLoggingKeyword(MICROSOFT_KEYWORD_MEASURES));
+        }
 
-		//ToDo if required add logging to disk file here 
-	}
+        //ToDo if required add logging to disk file here 
+    }
 }
