1b11ecc3a07a38b804eb7440bfcf09f4feb7904a
Yao Sun <yaosun@microsoft.com>
2020-01-31 18:20:59 -0800

    Refactor, manifest modifeied

--- src/AppInstallerRepositoryCore/Manifest/ManifestInstaller.cpp
+++ src/AppInstallerRepositoryCore/Manifest/ManifestInstaller.cpp
@@ -13,13 +13,18 @@ namespace AppInstaller::Manifest
         this->Url = installerNode["Url"].as<std::string>();
         this->Sha256 = Utility::SHA256::ConvertToBytes(installerNode["Sha256"].as<std::string>());
 
+        if (installerNode["SignatureSha256"])
+        {
+            this->SignatureSha256 = Utility::SHA256::ConvertToBytes(installerNode["SignatureSha256"].as<std::string>());
+        }
+
         // Optional fields.
         this->Language = installerNode["Language"] ? installerNode["Language"].as<std::string>() : "";
         this->Scope = installerNode["Scope"] ? installerNode["Scope"].as<std::string>() : "";
 
         this->InstallerType = installerNode["InstallerType"] ?
-            installerNode["InstallerType"].as<std::string>() :
-            defaultInstaller.InstallerType;
+            ConvertToInstallerTypeEnum(installerNode["InstallerType"].as<std::string>()) :
+            InstallerTypeEnum::Unknown;
 
         if (installerNode["Switches"])
         {
@@ -33,5 +38,29 @@ namespace AppInstaller::Manifest
         {
             this->Switches.emplace(defaultInstaller.Switches.value());
         }
+    }
+
+    ManifestInstaller::InstallerTypeEnum ManifestInstaller::ConvertToInstallerTypeEnum(const std::string& in)
+    {
+        static std::map<std::string, InstallerTypeEnum> InstallerTypeEnumMap = {
+            { "inno", InstallerTypeEnum::Inno },
+            { "wix", InstallerTypeEnum::Wix },
+            { "msi", InstallerTypeEnum::Msi },
+            { "nullsoft", InstallerTypeEnum::Nullsoft },
+            { "zip", InstallerTypeEnum::Zip },
+            { "appx", InstallerTypeEnum::Msix },
+            { "msix", InstallerTypeEnum::Msix },
+            { "exe", InstallerTypeEnum::Exe },
+        };
+
+        std::string inStrLower = Utility::ToLower(in);
+        auto iter = InstallerTypeEnumMap.find(inStrLower);
+
+        if (iter != InstallerTypeEnumMap.end())
+        {
+            return iter->second;
+        }
+
+        return InstallerTypeEnum::Unknown;
     }
 }
