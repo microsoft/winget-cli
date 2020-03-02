71a80cd607fe7fcddd6dd0b361e2c0adb36e03e6
John McPherson <johnmcp@microsoft.com>
2020-03-02 13:57:26 -0800

    Add comment on source's expectations for Arg and Data

--- src/AppInstallerRepositoryCore/Microsoft/PreIndexedPackageSourceFactory.h
+++ src/AppInstallerRepositoryCore/Microsoft/PreIndexedPackageSourceFactory.h
@@ -10,6 +10,12 @@ namespace AppInstaller::Repository::Microsoft
 {
     // A source where the index is precomputed and stored on a server within an optional MSIX package.
     // In addition, the manifest files are also individually available on the server.
+    // Arg  ::  Expected to be a fully qualified path to the root of the data.
+    //          This can be a web location such as https://somewhere/ or a local file share \\somewhere\
+    //          Under this path there must exist an MSIX package called "index.msix".
+    //          This must have a file called "index.db" contained within, which is a SQLiteIndex.
+    //          The index's paths refer to relative locations under the Arg value.
+    // Data ::  The package family name of the package at Arg + /index.msix.
     struct PreIndexedPackageSourceFactory
     {
         // Get the type string for this source.
