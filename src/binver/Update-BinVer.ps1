a01e908e203747179b00de3bf2a66b237761a02d
John McPherson <johnmcp@microsoft.com>
2020-03-20 15:12:05 -0700

    Skip codesign and ignore errors from git

--- src/binver/Update-BinVer.ps1
+++ src/binver/Update-BinVer.ps1
@@ -17,7 +17,7 @@ param(
     [int]$BuildVersion = 0
 )
 
-$Local:GitDescribeText = git describe --tags;
+$Local:GitDescribeText = (git describe --tags 2> $null);
 Write-Host "Git describe: $Local:GitDescribeText"
 
 $Local:Major = 0;
