76d92152cb131572a1d5dbbec98e04f33bd2c985
JohnMcPMS <johnmcp@microsoft.com>
2020-04-06 23:45:37 -0700

    Show wil exceptions to the user in a cleaner fashion (#80)

--- src/AppInstallerCommonCore/Public/AppInstallerErrors.h
+++ src/AppInstallerCommonCore/Public/AppInstallerErrors.h
@@ -1,17 +1,22 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-
 #pragma once
+#include <wil/result_macros.h>
+#include <winrt/base.h>
 
-#define APPINSTALLER_CLI_ERROR_FACILITY 0x8A150000
+#include <exception>
+#include <string>
+
+
+#define APPINSTALLER_CLI_ERROR_FACILITY 0xA15
 
 #define APPINSTALLER_CLI_ERROR_INTERNAL_ERROR                   ((HRESULT)0x8A150001)
 #define APPINSTALLER_CLI_ERROR_INVALID_CL_ARGUMENTS             ((HRESULT)0x8A150002)
 #define APPINSTALLER_CLI_ERROR_COMMAND_FAILED                   ((HRESULT)0x8A150003)
 #define APPINSTALLER_CLI_ERROR_MANIFEST_FAILED                  ((HRESULT)0x8A150004)
-#define APPINSTALLER_CLI_ERROR_WORKFLOW_FAILED                  ((HRESULT)0x8A150005)
+//#define APPINSTALLER_CLI_ERROR_WORKFLOW_FAILED                  ((HRESULT)0x8A150005) // Unused, can be repurposed
 #define APPINSTALLER_CLI_ERROR_SHELLEXEC_INSTALL_FAILED         ((HRESULT)0x8A150006)
-#define APPINSTALLER_CLI_ERROR_RUNTIME_ERROR                    ((HRESULT)0x8A150007)
+//#define APPINSTALLER_CLI_ERROR_RUNTIME_ERROR                    ((HRESULT)0x8A150007) // Unused, can be repurposed
 #define APPINSTALLER_CLI_ERROR_DOWNLOAD_FAILED                  ((HRESULT)0x8A150008)
 #define APPINSTALLER_CLI_ERROR_CANNOT_WRITE_TO_UPLEVEL_INDEX    ((HRESULT)0x8A150009)
 #define APPINSTALLER_CLI_ERROR_INDEX_INTEGRITY_COMPROMISED      ((HRESULT)0x8A15000A)
@@ -28,3 +33,11 @@
 #define APPINSTALLER_CLI_ERROR_NO_SOURCES_DEFINED               ((HRESULT)0x8A150015)
 #define APPINSTALLER_CLI_ERROR_MULTIPLE_APPLICATIONS_FOUND      ((HRESULT)0x8A150016)
 #define APPINSTALLER_CLI_ERROR_NO_MANIFEST_FOUND                ((HRESULT)0x8A150017)
+
+namespace AppInstaller
+{
+    // Gets error messages that are presentable to the user.
+    std::string GetUserPresentableMessage(const wil::ResultException& re);
+    std::string GetUserPresentableMessage(const winrt::hresult_error& hre);
+    std::string GetUserPresentableMessage(const std::exception& e);
+}
