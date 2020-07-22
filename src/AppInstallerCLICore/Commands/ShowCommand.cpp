caa6256125f58e5422545b16bac191cd5dc22932
Marcus Kern <marcus@sorafamily.com>
2020-07-22 11:13:25 -0500

    adjusted show manifest info method to test for Installer prior to attempting to get it (cause issue when installer wasn't present in the context)

--- src/AppInstallerCLICore/Commands/ShowCommand.cpp
+++ src/AppInstallerCLICore/Commands/ShowCommand.cpp
@@ -71,6 +71,7 @@ namespace AppInstaller::CLI
         	{
                 context <<
                     Workflow::GetManifest <<
+                    Workflow::ShowManifestInfo <<
                     Workflow::GetRequestedUrlFromManifest <<
                     Workflow::OpenUrlInDefaultBrowser;
             }
