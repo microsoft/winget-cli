a4ea30b74011bed054186f99a91f4ac651eff7dc
Ryan Fu <ryfu@microsoft.com>
2020-09-01 16:19:19 -0700

    revert powershell script to start-process background

--- src/IndexHostService/Run-IndexHostService.ps1
+++ src/IndexHostService/Run-IndexHostService.ps1
@@ -27,5 +27,4 @@ param(
 
 cd $BuildRoot
 
-IndexHostService.exe -d $LocalIndexRoot -c $CertPath -p $CertPassword 
-#Start-Process -FilePath "IndexHostService.exe" -ArgumentList "-d $LocalIndexRoot -c $CertPath -p $CertPassword" 
+Start-Process -FilePath "IndexHostService.exe" -ArgumentList "-d $LocalIndexRoot -c $CertPath -p $CertPassword" 
