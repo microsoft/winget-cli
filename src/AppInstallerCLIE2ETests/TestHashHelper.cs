d5093d298c413b3061e07243275f410d860685d4
Ryan Fu <ryfu@microsoft.com>
2020-09-10 11:08:20 -0700

    define constant

--- src/AppInstallerCLIE2ETests/TestHashHelper.cs
+++ src/AppInstallerCLIE2ETests/TestHashHelper.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System;
 using System.IO;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace AppInstallerCLIE2ETests
 {
--- src/AppInstallerCLIE2ETests/TestIndexSetup.cs
+++ src/AppInstallerCLIE2ETests/TestIndexSetup.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System;
 using System.Diagnostics;
 using System.IO;
 
