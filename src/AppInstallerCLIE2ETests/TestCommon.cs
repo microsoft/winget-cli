e5c41cf35ebb4d98f762fe4546bff8d1d1b99d1e
Ryan Fu <ryfu@microsoft.com>
2020-10-14 17:29:16 -0700

    changed x86 log path

--- src/AppInstallerCLIE2ETests/TestCommon.cs
+++ src/AppInstallerCLIE2ETests/TestCommon.cs
@@ -243,7 +243,6 @@ namespace AppInstallerCLIE2ETests
                 if (Directory.Exists(testLogsDestPath))
                 {
                     TestIndexSetup.DeleteDirectoryContents(new DirectoryInfo(testLogsDestPath));
-                    Directory.Delete(testLogsDestPath);
                 }
                 TestIndexSetup.CopyDirectory(testLogsSourcePath, testLogsDestPath);
             }
