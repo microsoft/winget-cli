6a598a98693964d1c7e0f1bfe56e96bc0850e4fa
Yao Sun <yaosun@microsoft.com>
2021-01-23 19:24:31 -0800

    vcxitems for schemas

---
+++ src/ManifestSchema/ManifestSchema.h
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+
+#define MANIFESTSCHEMA_RESOURCE_TYPE                  200
+
+#define IDX_MANIFEST_SCHEMA_PREVIEW                   201
+#define IDX_MANIFEST_SCHEMA_V1_SINGLETON              202
+#define IDX_MANIFEST_SCHEMA_V1_VERSION                203
+#define IDX_MANIFEST_SCHEMA_V1_INSTALLER              204
+#define IDX_MANIFEST_SCHEMA_V1_DEFAULTLOCALE          205
+#define IDX_MANIFEST_SCHEMA_V1_LOCALE                 206
