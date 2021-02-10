d89c0b8c5422deff897aea5d53e5d033529942e6
Ashwini Patil <ashpatil@microsoft.com>
2021-02-09 16:46:51 -0800

    Add basic structs needed for Rest source

---
+++ src/AppInstallerRepositoryCore/RestClient.h
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+
+namespace AppInstaller::Repository::Rest
+{
+	struct RestClient
+	{
+		RestClient() {}
+	};
+}
