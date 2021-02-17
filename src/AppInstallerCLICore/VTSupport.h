f20e00253f2f2829d6e07f8f703fbaca34bbc257
Martin Strobel <marstr@microsoft.com>
2021-02-17 13:55:56 -0800

    Add Hyperlink function to ViritualTerminal TextFormat namespace

--- src/AppInstallerCLICore/VTSupport.h
+++ src/AppInstallerCLICore/VTSupport.h
@@ -123,6 +123,8 @@ namespace AppInstaller::CLI::VirtualTerminal
         {
 
         }
+
+        ConstructedSequence Hyperlink(const std::string& link, const std::string& text);
     }
 
     namespace TextModification
