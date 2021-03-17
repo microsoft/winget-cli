6600da1db51f4e541dc36be0c97388aee4b0dfd8
sachintaMSFT <80828309+sachintaMSFT@users.noreply.github.com>
2021-03-17 06:19:56 -0700

    Update AppInstallerCLICore.h

--- src/AppInstallerCLICore/Public/AppInstallerCLICore.h
+++ src/AppInstallerCLICore/Public/AppInstallerCLICore.h
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 #pragma once
+#include "Caller.h"
+#include <AppInstallerProgress.h>
 
 namespace AppInstaller::CLI
 {
     int CoreMain(int argc, wchar_t const** argv);
+	int Install(std::wstring_view appToInstall, AppInstallerCaller caller, IProgressSink& comPS);
 }
