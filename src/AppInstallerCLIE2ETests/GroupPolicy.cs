0c496995b55ea90a4423e871f468af0f64dd3903
Luis Enrique Chacon Ochoa <luchacon@microsoft.com>
2021-04-05 15:13:46 -0700

    Add Group Policy test helper

---
+++ src/AppInstallerCLIE2ETests/GroupPolicy.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AppInstallerCLIE2ETests
+{
+    /// <summary>
+    /// Tests for enforcement of Group Policy.
+    /// </summary>
+    public class GroupPolicy : BaseCommand
+    {
+    }
+}
