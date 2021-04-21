7c8807471a15128b81fb72abe00044a1f4e9c5f4
JohnMcPMS <johnmcp@microsoft.com>
2021-04-20 17:04:54 -0700

    Move import out of experimental (#881)

--- src/binver/binver/version.h
+++ src/binver/binver/version.h
@@ -2,7 +2,7 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               0
-#define VERSION_MINOR               2
+#define VERSION_MINOR               3
 #define VERSION_BUILD               0
 #define VERSION_REVISION            0
 
