dc44aa525b96d601739951a694d1e4ceb16f72c3
JohnMcPMS <johnmcp@microsoft.com>
2021-09-21 11:44:17 -0700

    Change to filters for rest

--- src/AppInstallerCLICore/Workflows/ImportExportFlow.cpp
+++ src/AppInstallerCLICore/Workflows/ImportExportFlow.cpp
@@ -271,7 +271,7 @@ namespace AppInstaller::CLI::Workflow
 
                 // Search for the current package
                 SearchRequest searchRequest;
-                searchRequest.Inclusions.emplace_back(PackageMatchFilter(PackageMatchField::Id, MatchType::CaseInsensitive, packageRequest.Id.get()));
+                searchRequest.Filters.emplace_back(PackageMatchFilter(PackageMatchField::Id, MatchType::CaseInsensitive, packageRequest.Id.get()));
 
                 auto searchContextPtr = context.Clone();
                 Execution::Context& searchContext = *searchContextPtr;
