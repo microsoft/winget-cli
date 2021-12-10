1cf5cf02d8b5643d0bca14a96579bd9c12b7606c
denelon <denelon@microsoft.com>
2021-12-10 15:29:10 -0800

    tabs vs. spaces

--- tools/PowerShell/Microsoft.WinGet.Client/src/Microsoft.WinGet.Client.psm1
+++ tools/PowerShell/Microsoft.WinGet.Client/src/Microsoft.WinGet.Client.psm1
@@ -4,9 +4,9 @@ $RequiredVersion = "1.1.12653"
 
 $version = Get-WinGetVersion
 if ($version -lt $RequiredVersion) {
-	# Need to do localization
+    # Need to do localization
     Write-Host "Windows Package Manager is missing. For more information on installing the Windows Package Manager. `n  Visit: https://github.com/microsoft/winget-cli#installing-the-client"
-	throw [WinGetVersionMismatch]::new("Requires Windows Package Manager $RequiredVersion or later to be installed.")
+    throw [WinGetVersionMismatch]::new("Requires Windows Package Manager $RequiredVersion or later to be installed.")
 }
 
 Get-ChildItem -Path $PSScriptRoot\Library -Filter *.ps1 | foreach-object { . $_.FullName }
