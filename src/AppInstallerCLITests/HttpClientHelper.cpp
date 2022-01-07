73ed58700fcbb116d9ce5e6b55dd412f9a2177c8
JohnMcPMS <johnmcp@microsoft.com>
2022-01-07 12:12:39 -0800

    Add a default user agent to REST source calls

--- src/AppInstallerCLITests/HttpClientHelper.cpp
+++ src/AppInstallerCLITests/HttpClientHelper.cpp
@@ -5,8 +5,12 @@
 #include "TestRestRequestHandler.h"
 #include <AppInstallerErrors.h>
 #include <Rest/Schema/HttpClientHelper.h>
+#include <AppInstallerRuntime.h>
+#include <AppInstallerStrings.h>
 
 using namespace AppInstaller::Repository::Rest::Schema;
+using namespace AppInstaller::Runtime;
+using namespace AppInstaller::Utility;
 
 TEST_CASE("ExtractJsonResponse_UnsupportedMimeType", "[RestSource][RestSearch]")
 {
@@ -24,4 +28,29 @@ TEST_CASE("ValidateAndExtractResponse_NotFound", "[RestSource]")
 {
     HttpClientHelper helper{ GetTestRestRequestHandler(web::http::status_codes::NotFound) };
     REQUIRE_THROWS_HR(helper.HandleGet(L"https://testUri"), APPINSTALLER_CLI_ERROR_RESTSOURCE_ENDPOINT_NOT_FOUND);
+}
+
+TEST_CASE("EnsureDefaultUserAgent", "[RestSource]")
+{
+    HttpClientHelper helper{ GetTestRestRequestHandler([](const web::http::http_request& request)
+        {
+            auto itr = request.headers().find(web::http::header_names::user_agent);
+            if (itr != request.headers().end() && itr->second.find(ConvertToUTF16(GetClientVersion())) != utility::string_t::npos)
+            {
+                return web::http::status_codes::OK;
+            }
+            else
+            {
+                return web::http::status_codes::BadRequest;
+            }
+        }) };
+
+    SECTION("GET")
+    {
+        REQUIRE_NOTHROW(helper.HandleGet(L"https://testUri"));
+    }
+    SECTION("POST")
+    {
+        REQUIRE_NOTHROW(helper.HandlePost(L"https://testUri", {}));
+    }
 }
