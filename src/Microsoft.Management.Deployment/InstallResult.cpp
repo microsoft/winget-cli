fb4ef15bc88f24b5e0cbb460d89a0963066f38fd
JohnMcPMS <johnmcp@microsoft.com>
2022-02-10 14:43:48 -0800

    Add InstallerErrorCode to COM interface

--- src/Microsoft.Management.Deployment/InstallResult.cpp
+++ src/Microsoft.Management.Deployment/InstallResult.cpp
@@ -9,12 +9,14 @@ namespace winrt::Microsoft::Management::Deployment::implementation
 {
     void InstallResult::Initialize(
         winrt::Microsoft::Management::Deployment::InstallResultStatus status,
-        winrt::hresult extendedErrorCode, 
+        winrt::hresult extendedErrorCode,
+        uint32_t installerErrorCode,
         hstring const& correlationData, 
         bool rebootRequired)
     {
         m_status = status;
         m_extendedErrorCode = extendedErrorCode;
+        m_installerErrorCode = installerErrorCode;
         m_correlationData = correlationData;
         m_rebootRequired = rebootRequired;
     }
@@ -33,5 +35,10 @@ namespace winrt::Microsoft::Management::Deployment::implementation
     winrt::hresult InstallResult::ExtendedErrorCode()
     {
         return m_extendedErrorCode;
+    }
+
+    uint32_t InstallResult::InstallerErrorCode()
+    {
+        return m_installerErrorCode;
     }
 }
