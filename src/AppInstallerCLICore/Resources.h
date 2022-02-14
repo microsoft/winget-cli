dcfa80813d8acfef218069a56774cc378729d391
Easton Pillay <easton@planeteaston.com>
2022-02-14 09:52:48 -0500

    Added system architecture to `winget --info`.

--- src/AppInstallerCLICore/Resources.h
+++ src/AppInstallerCLICore/Resources.h
@@ -27,6 +27,7 @@ namespace AppInstaller::CLI::Resource
         WINGET_DEFINE_RESOURCE_STRINGID(AdminSettingDisableDescription);
         WINGET_DEFINE_RESOURCE_STRINGID(AdminSettingEnabled);
         WINGET_DEFINE_RESOURCE_STRINGID(AdminSettingEnableDescription);
+        WINGET_DEFINE_RESOURCE_STRINGID(Architecture);
         WINGET_DEFINE_RESOURCE_STRINGID(AvailableArguments);
         WINGET_DEFINE_RESOURCE_STRINGID(AvailableCommands);
         WINGET_DEFINE_RESOURCE_STRINGID(AvailableHeader);
