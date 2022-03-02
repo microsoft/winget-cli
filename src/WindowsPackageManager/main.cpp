7608673d5ee1b99eee8b553a2e6d4273a0354cb6
Yao Sun <yaosun@microsoft.com>
2022-03-01 16:56:54 -0800

    fix debug build

--- src/WindowsPackageManager/main.cpp
+++ src/WindowsPackageManager/main.cpp
@@ -1,7 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 #include <wil/result_macros.h>
+#pragma warning( push )
+#pragma warning ( disable : 4324 )
 #include <wrl/module.h>
+#pragma warning( pop )
 #include <winrt/Microsoft.Management.Deployment.h>
 
 #include "WindowsPackageManager.h"
