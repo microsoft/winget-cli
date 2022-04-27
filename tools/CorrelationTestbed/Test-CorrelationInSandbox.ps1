aaf2abc97d8c2eef804d60a0a6597e55fb90d740
JohnMcPMS <johnmcp@microsoft.com>
2022-04-27 14:24:32 -0700

    Add -Wait parameter to make manual testing usage a little easier

--- tools/CorrelationTestbed/Test-CorrelationInSandbox.ps1
+++ tools/CorrelationTestbed/Test-CorrelationInSandbox.ps1
@@ -15,7 +15,9 @@ Param(
   [Parameter(HelpMessage = "The results output path.")]
   [String] $ResultsPath,
   [Parameter(HelpMessage = "The path to registry files that should be injected before the test.")]
-  [String] $RegFileDirectory
+  [String] $RegFileDirectory,
+  [Parameter(HelpMessage = "Wait for user input before tearing down each sandbox.")]
+  [Switch] $Wait
 )
 
 $ErrorActionPreference = "Stop"
@@ -348,6 +350,11 @@ foreach ($packageIdentifier in $PackageIdentifiers)
           break
         }
         Start-Sleep 1
+    }
+
+    if ($Wait)
+    {
+        Read-Host "Press Enter to close sandbox and continue..."
     }
 
     Close-WindowsSandbox
