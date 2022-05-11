1b120fd8d9085d9db97cbcc7a999c11beb401fd1
Ryan Fu <Fu.Ryan@microsoft.com>
2022-05-11 15:16:06 -0700

    Add cleanup to arp mismatch tests

--- src/AppInstallerCLIE2ETests/UpgradeCommand.cs
+++ src/AppInstallerCLIE2ETests/UpgradeCommand.cs
@@ -33,42 +33,26 @@ namespace AppInstallerCLIE2ETests
         }
 
         [Test]
-        public void UpgradePortableUninstallPrevious()
-        {
-            string installDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet", "Packages");
-            string packageId, commandAlias, fileName, packageDirName, productCode;
-            packageId = "AppInstallerTest.TestPortableExe";
-            packageDirName = productCode = packageId + "_CompositeSource";
-            commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
-
-            var result = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe -v 1.0.0.0");
-            Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            Assert.True(result.StdOut.Contains("Successfully installed"));
-
-            var result2 = TestCommon.RunAICLICommand("upgrade", "AppInstallerTest.TestPortableExe -v 3.0.0.0");
-            Assert.AreEqual(Constants.ErrorCode.S_OK, result2.ExitCode);
-            Assert.True(result2.StdOut.Contains("Successfully installed"));
-            TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, true);
-        }
-
-        [Test]
         public void UpgradePortableARPMismatch()
         {
             string packageId = "AppInstallerTest.TestPortableExe";
             string productCode = packageId + "_" + Constants.TestSourceIdentifier;
 
-            var result = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe -v 1.0.0.0");
-            Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            Assert.True(result.StdOut.Contains("Successfully installed"));
+            var installResult = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe -v 1.0.0.0");
+            Assert.AreEqual(Constants.ErrorCode.S_OK, installResult.ExitCode);
+            Assert.True(installResult.StdOut.Contains("Successfully installed"));
 
-            // Modify packageId and sourceId to cause mismatch.
+            // Modify packageId to cause mismatch.
             ModifyPortableARPEntryValue(productCode, WinGetPackageIdentifier, "testPackageId");
-            ModifyPortableARPEntryValue(productCode, WinGetSourceIdentifier, "testPackageId");
 
-            var result2 = TestCommon.RunAICLICommand("upgrade", "AppInstallerTest.TestPortableExe -v 2.0.0.0");
-            DeleteTestPortableARPEntry(productCode);
-            Assert.AreNotEqual(Constants.ErrorCode.S_OK, result2.ExitCode);
-            Assert.True(result2.StdOut.Contains("Portable package from a different source already exists."));
+            var upgradeResult = TestCommon.RunAICLICommand("upgrade", "AppInstallerTest.TestPortableExe -v 2.0.0.0");
+
+            // Reset and perform uninstall cleanup
+            ModifyPortableARPEntryValue(productCode, WinGetPackageIdentifier, packageId);
+            TestCommon.RunAICLICommand("uninstall", $"--product-code {productCode}");
+
+            Assert.AreNotEqual(Constants.ErrorCode.S_OK, upgradeResult.ExitCode);
+            Assert.True(upgradeResult.StdOut.Contains("Portable package from a different source already exists"), $"Actual output message is: {upgradeResult.StdOut}");
         }
 
         [Test]
@@ -80,15 +64,34 @@ namespace AppInstallerCLIE2ETests
             packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
             commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
 
+            var installResult = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe -v 1.0.0.0");
+            Assert.AreEqual(Constants.ErrorCode.S_OK, installResult.ExitCode);
+            Assert.True(installResult.StdOut.Contains("Successfully installed"));
+
+            // Modify packageId and sourceId to cause mismatch.
+            ModifyPortableARPEntryValue(productCode, WinGetPackageIdentifier, "testPackageId");
+            ModifyPortableARPEntryValue(productCode, WinGetSourceIdentifier, "testSourceId");
+
+            var upgradeResult = TestCommon.RunAICLICommand("upgrade", $"--id {productCode} -v 2.0.0.0 --force");
+            Assert.AreEqual(Constants.ErrorCode.S_OK, upgradeResult.ExitCode);
+            Assert.True(upgradeResult.StdOut.Contains("Successfully installed"));
+            TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, true);
+        }
+
+        [Test]
+        public void UpgradePortableUninstallPrevious()
+        {
+            string installDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet", "Packages");
+            string packageId, commandAlias, fileName, packageDirName, productCode;
+            packageId = "AppInstallerTest.TestPortableExe";
+            packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
+            commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
+
             var result = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe -v 1.0.0.0");
             Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
             Assert.True(result.StdOut.Contains("Successfully installed"));
 
-            // Modify packageId and sourceId to cause mismatch.
-            ModifyPortableARPEntryValue(productCode, WinGetPackageIdentifier, "testPackageId");
-            ModifyPortableARPEntryValue(productCode, WinGetSourceIdentifier, "testPackageId");
-
-            var result2 = TestCommon.RunAICLICommand("upgrade", "AppInstallerTest.TestPortableExe -v 2.0.0.0 --force");
+            var result2 = TestCommon.RunAICLICommand("upgrade", $"--id {productCode} -v 3.0.0.0");
             Assert.AreEqual(Constants.ErrorCode.S_OK, result2.ExitCode);
             Assert.True(result2.StdOut.Contains("Successfully installed"));
             TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, true);
