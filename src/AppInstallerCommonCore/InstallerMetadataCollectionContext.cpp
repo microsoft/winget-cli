ec7cb5ecb29ebdcc1fa2b8a326619deae12f9f5e
JohnMcPMS <johnmcp@microsoft.com>
2022-05-18 21:35:27 -0700

    Needs refactoring to expose JSON manifest parsing

--- src/AppInstallerCommonCore/InstallerMetadataCollectionContext.cpp
+++ src/AppInstallerCommonCore/InstallerMetadataCollectionContext.cpp
@@ -4,6 +4,7 @@
 #include "Public/winget/InstallerMetadataCollectionContext.h"
 
 #include "Public/AppInstallerDownloader.h"
+#include "Public/AppInstallerErrors.h"
 #include "Public/AppInstallerLogging.h"
 #include "Public/AppInstallerStrings.h"
 
@@ -85,6 +86,23 @@ namespace AppInstaller::Utility
         return result;
     }
 
+    void InstallerMetadataCollectionContext::Complete(const std::filesystem::path& output, const std::filesystem::path& diagnostics)
+    {
+        auto threadGlobalsLifetime = m_threadGlobals.SetForCurrentThread();
+
+        THROW_HR_IF(E_INVALIDARG, output.empty());
+
+        UNREFERENCED_PARAMETER(diagnostics);
+
+        // Collect post-install system state
+
+        // Compute metadata match scores
+
+        // Write output
+
+        // Write diagnostics
+    }
+
     std::unique_ptr<ThreadLocalStorage::PreviousThreadGlobals> InstallerMetadataCollectionContext::InitializeLogging(const std::filesystem::path& logFile)
     {
         auto threadGlobalsLifetime = m_threadGlobals.SetForCurrentThread();
@@ -107,28 +125,92 @@ namespace AppInstaller::Utility
 
     void InstallerMetadataCollectionContext::InitializePreinstallState(const std::wstring& json)
     {
-        // Parse and validate JSON
-        web::json::value inputValue = web::json::value::parse(json);
+        AICLI_LOG(Core, Verbose, << "Parsing input JSON:\n" << ConvertToUTF8(json));
 
-        //if (inputValue.has_field)
+        // Parse and validate JSON
+        bool jsonException = false;
+
+        try
+        {
+            utility::string_t versionFieldName = L"version";
+
+            web::json::value inputValue = web::json::value::parse(json);
+
+            THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, inputValue.is_null());
+
+            auto versionString = JSON::GetRawStringValueFromJsonNode(inputValue, versionFieldName);
+            THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, !versionString);
+
+            Version version{ versionString.value() };
+            AICLI_LOG(Core, Info, << "Parsing input JSON version " << version.ToString());
+
+            if (version.GetParts()[0].Integer == 1)
+            {
+                // We only have one version currently, so use that as long as the major version is 1
+                ParseInputJson_1_0(inputValue);
+            }
+        }
+        catch (const web::json::json_exception& exc)
+        {
+            AICLI_LOG(Core, Error, << "Exception parsing input JSON: " << exc.what());
+            jsonException = true;
+        }
+
+        THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, jsonException);
 
         // Collect pre-install system state
     }
 
-    void InstallerMetadataCollectionContext::Complete(const std::filesystem::path& output, const std::filesystem::path& diagnostics)
+    void InstallerMetadataCollectionContext::ParseInputJson_1_0(web::json::value& input)
     {
-        auto threadGlobalsLifetime = m_threadGlobals.SetForCurrentThread();
+        AICLI_LOG(Core, Info, << "Parsing input JSON 1.0 fields");
 
-        THROW_HR_IF(E_INVALIDARG, output.empty());
+        // Field names
+        utility::string_t blobVersionFieldName = L"supportedBlobVersion";
+        utility::string_t blobSizeFieldName = L"maximumBlobSize";
+        utility::string_t blobFieldName = L"currentBlob";
+        utility::string_t productRevisionFieldName = L"productRevision";
+        utility::string_t installerHashFieldName = L"installerHash";
+        utility::string_t currentManifestFieldName = L"currentManifest";
+        utility::string_t incomingManifestFieldName = L"incomingManifest";
 
-        UNREFERENCED_PARAMETER(diagnostics);
+        auto blobVersionString = JSON::GetRawStringValueFromJsonNode(input, blobVersionFieldName);
+        THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, !blobVersionString);
+        m_supportedBlobVersion = Version{ blobVersionString.value() };
 
-        // Collect post-install system state
+        auto blobSizeNumber = JSON::GetRawIntValueFromJsonNode(input, blobSizeFieldName);
+        if (blobSizeNumber && blobSizeNumber.value() > 0)
+        {
+            m_maxBlobSize = static_cast<size_t>(blobSizeNumber.value());
+        }
+        else
+        {
+            m_maxBlobSize = std::numeric_limits<size_t>::max();
+        }
 
-        // Compute metadata match scores
+        auto currentBlobValue = JSON::GetJsonValueFromNode(input, blobFieldName);
+        if (currentBlobValue)
+        {
+            // TODO: Create and parse blob here
+            m_currentBlob = currentBlobValue.value();
+        }
 
-        // Write output
+        auto productRevisionNumber = JSON::GetRawIntValueFromJsonNode(input, productRevisionFieldName);
+        THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, !productRevisionNumber);
+        m_productRevision = productRevisionNumber.value();
 
-        // Write diagnostics
+        auto installerHashString = JSON::GetRawStringValueFromJsonNode(input, installerHashFieldName);
+        THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, !installerHashString);
+        m_installerHash = SHA256::ConvertToBytes(installerHashString.value());
+
+        auto currentManifestValue = JSON::GetJsonValueFromNode(input, currentManifestFieldName);
+        if (currentManifestValue)
+        {
+            //m_currentManifest = 
+        }
+
+        auto incomingManifestValue = JSON::GetJsonValueFromNode(input, incomingManifestFieldName);
+        THROW_HR_IF(APPINSTALLER_CLI_ERROR_JSON_INVALID_FILE, !incomingManifestValue);
+        // m_incomingManifest = 
     }
 }
--- src/AppInstallerCommonCore/Public/winget/InstallerMetadataCollectionContext.h
+++ src/AppInstallerCommonCore/Public/winget/InstallerMetadataCollectionContext.h
@@ -2,12 +2,17 @@
 // Licensed under the MIT License.
 #pragma once
 
+#include <Public/AppInstallerVersions.h>
+#include <Public/AppInstallerSHA256.h>
+#include <winget/Manifest.h>
+#include <winget/JsonUtil.h>
+#include <winget/ThreadGlobals.h>
+
 #include <filesystem>
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
-
-#include <winget/ThreadGlobals.h>
 
 namespace AppInstaller::Utility
 {
@@ -37,6 +42,18 @@ namespace AppInstaller::Utility
         // Sets the collection context input and the preinstall state.
         void InitializePreinstallState(const std::wstring& json);
 
+        // Parse version 1.0 of input JSON
+        void ParseInputJson_1_0(web::json::value& input);
+
         ThreadLocalStorage::ThreadGlobals m_threadGlobals;
+
+        // Parsed input
+        Version m_supportedBlobVersion;
+        size_t m_maxBlobSize = 0;
+        web::json::value m_currentBlob; // TODO: Parse blob as well
+        int m_productRevision = 0;
+        SHA256::HashBuffer m_installerHash;
+        Manifest::Manifest m_currentManifest;
+        Manifest::Manifest m_incomingManifest;
     };
 }
