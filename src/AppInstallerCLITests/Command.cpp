5bdc55ea4aa66877694133c96763fba46381e1dd
Kaleb Luedtke <jluedtk@jci.com>
2022-06-16 12:28:12 -0500

    Enable long aliases (#2107)
    This change allows for argument aliases to be more than 1 character

--- src/AppInstallerCLITests/Command.cpp
+++ src/AppInstallerCLITests/Command.cpp
@@ -23,6 +23,18 @@ std::string GetArgumentName(const Argument& arg)
     return std::string{ arg.Name() };
 }
 
+std::string GetArgumentAlternateName(const Argument& arg)
+{
+    if (arg.AlternateName() == Argument::NoAlternateName)
+    {
+        return {};
+    }
+    else
+    {
+        return std::string{ arg.AlternateName() };
+    }
+}
+
 std::string GetArgumentAlias(const Argument& arg)
 {
     if (arg.Alias() == Argument::NoAlias)
@@ -36,10 +48,9 @@ std::string GetArgumentAlias(const Argument& arg)
 }
 
 template <typename Enumerable, typename Op>
-void EnsureStringsAreLowercaseAndNoCollisions(const std::string& info, const Enumerable& e, Op& op, bool requireLower = true)
+void EnsureStringsAreLowercaseAndNoCollisions(const std::string& info, const Enumerable& e, Op& op, std::unordered_set<std::string>& values, bool requireLower = true)
 {
     INFO(info);
-    std::unordered_set<std::string> values;
 
     for (const auto& val : e)
     {
@@ -62,13 +73,25 @@ void EnsureStringsAreLowercaseAndNoCollisions(const std::string& info, const Enu
     }
 }
 
+template <typename Enumerable, typename Op>
+void EnsureStringsAreLowercaseAndNoCollisions(const std::string& info, const Enumerable& e, Op& op, bool requireLower = true)
+{
+    std::unordered_set<std::string> values;
+    EnsureStringsAreLowercaseAndNoCollisions(info, e, op, values, requireLower);
+}
+
 void EnsureCommandConsistency(const Command& command)
 {
     EnsureStringsAreLowercaseAndNoCollisions(command.FullName() + " commands", command.GetCommands(), GetCommandName);
 
     auto args = command.GetArguments();
     Argument::GetCommon(args);
-    EnsureStringsAreLowercaseAndNoCollisions(command.FullName() + " argument names", args, GetArgumentName);
+
+    // Argument names and alternate names exist in the same space, so both need to be checked as a set
+    std::unordered_set<std::string> allArgumentNames;
+    EnsureStringsAreLowercaseAndNoCollisions(command.FullName() + " argument names", args, GetArgumentName, allArgumentNames);
+    EnsureStringsAreLowercaseAndNoCollisions(command.FullName() + " argument alternate name", args, GetArgumentAlternateName, allArgumentNames);
+    // Argument aliases use a different space than the names and can be checked separately
     EnsureStringsAreLowercaseAndNoCollisions(command.FullName() + " argument alias", args, GetArgumentAlias, false);
 
     // No : allowed in commands
@@ -412,6 +435,23 @@ TEST_CASE("ParseArguments_NameWithAdjoinedValue", "[command]")
     command.ParseArguments(inv, args);
 
     RequireValueParsedToArg(values[0].substr(7), command.m_args[0], args);
+}
+
+TEST_CASE("ParseArguments_AlternateNameWithAdjoinedValue", "[command]")
+{
+    Args args;
+    TestCommand command({
+            Argument{ "pos1", 'p', "p1", Args::Type::Channel, DefaultDesc, ArgumentType::Positional},
+            Argument{ "std1", 's', Args::Type::Command, DefaultDesc, ArgumentType::Standard },
+            Argument{ "pos2", 'q', Args::Type::Count, DefaultDesc, ArgumentType::Positional },
+        });
+
+    std::vector<std::string> values{ "--p1=Val1" };
+    Invocation inv{ std::vector<std::string>(values) };
+
+    command.ParseArguments(inv, args);
+
+    RequireValueParsedToArg(values[0].substr(5), command.m_args[0], args);
 }
 
 TEST_CASE("ParseArguments_NameFlag", "[command]")
