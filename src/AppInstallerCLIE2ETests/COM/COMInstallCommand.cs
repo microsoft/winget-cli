a017115176dc303c9b249d2ae2376dcb2675c85d
Amir El Bawab <104940545+AmelBawa-msft@users.noreply.github.com>
2022-06-24 10:07:09 -0700

    Installer tests

--- src/AppInstallerCLIE2ETests/COM/COMInstallCommand.cs
+++ src/AppInstallerCLIE2ETests/COM/COMInstallCommand.cs
@@ -12,10 +12,9 @@ namespace AppInstallerCLIE2ETests
     using System.Threading.Tasks;
     using WinRT;
 
-    public class COMInstallCommand
+    public class COMInstallCommand : BaseCommand
     {
         private PackageManager packageManager;
-        private PackageCatalog owcSource;
         private string installDir;
 
         private const string TestPackageCatalog = "TestSource";
@@ -29,13 +28,6 @@ namespace AppInstallerCLIE2ETests
         {
             packageManager = new PackageManager();
             installDir = TestCommon.GetRandomTestDir();
-
-            // Get test source
-            var testSource = packageManager.GetPackageCatalogByName(TestPackageCatalog);
-            Assert.NotNull(testSource, $"Ensure that {TestPackageCatalog} is added");
-
-            // Connect to test source
-            owcSource = testSource.Connect().PackageCatalog;
         }   
 
         [Test]
@@ -122,172 +114,140 @@ namespace AppInstallerCLIE2ETests
         [Test]
         public async Task InstallMSIX()
         {
-            var searchResult = FindOnePackage(PackageMatchField.Name, PackageFieldMatchOption.Equals, "TestMsixInstaller");
-            var installResult = await Install(searchResult.CatalogPackage);
-            Assert.True(InstallResultStatus.Ok == installResult.Status);
-            Assert.True(VerifyTestMsixInstalledAndCleanup());
+            // FIXME
+            Assert.Ignore();
         }
 
         [Test]
         public async Task InstallMSIXWithSignature()
         {
-            var searchResult = FindOnePackage(PackageMatchField.Name, PackageFieldMatchOption.Equals, "TestMsixWithSignatureHash");
-            var installResult = await Install(searchResult.CatalogPackage);
-            Assert.True(InstallResultStatus.Ok == installResult.Status);
-            Assert.True(VerifyTestMsixInstalledAndCleanup());
+            // FIXME
+            Assert.Ignore();
         }
 
         [Test]
         public async Task InstallMSIXWithSignatureHashMismatch()
         {
-            var searchResult = FindOnePackage(PackageMatchField.Name, PackageFieldMatchOption.Equals, "TestMsixSignatureHashMismatch");
-            var installResult = await Install(searchResult.CatalogPackage);
-            
             // FIXME
-            Assert.True(InstallResultStatus.Ok == installResult.Status);
-            Assert.False(VerifyTestMsixInstalledAndCleanup());
+            Assert.Ignore();
         }
 
         [Test]
         public void InstallExeWithAlternateSourceFailure()
         {
-            // TestCommon.RunAICLICommand("source add", "failSearch \"{ \"\"SearchHR\"\": \"\"0x80070002\"\" }\" Microsoft.Test.Configurable --header \"{}\"");
+            TestCommon.RunAICLICommand("source add", "failSearch \"{ \"\"SearchHR\"\": \"\"0x80070002\"\" }\" Microsoft.Test.Configurable --header \"{}\"");
 
-            // try
-            // {
-            //     var installDir = TestCommon.GetRandomTestDir();
-            //     var result = TestCommon.RunAICLICommand("install", $"AppInstallerTest.TestExeInstaller --silent -l {installDir}");
-            //     Assert.AreEqual(unchecked((int)0x80070002), result.ExitCode);
-            //     Assert.True(result.StdOut.Contains("Failed when searching source: failSearch"));
-            //     Assert.True(result.StdOut.Contains("AppInstallerTest.TestExeInstaller"));
-            //     Assert.False(result.StdOut.Contains("Successfully installed"));
-            //     Assert.False(VerifyTestExeInstalled(installDir));
-            // }
-            // finally
-            // {
-            //     ResetTestSource();
-            // }
+            var searchResult = FindAllPackages(PackageMatchField.Id, PackageFieldMatchOption.Equals, "AppInstallerTest.TestExeInstaller", "failSearch");
+            Assert.True(0 == searchResult.Count);
+            ResetTestSource();
         }
 
         [Test]
-        public void InstallPortableExe()
+        public async Task InstallPortableExe()
         {
-            // string installDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet", "Packages");
-            // string packageId, commandAlias, fileName, packageDirName, productCode;
-            // packageId = "AppInstallerTest.TestPortableExe";
-            // packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
-            // commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
+            string installDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet", "Packages");
+            string packageId, commandAlias, fileName, packageDirName, productCode;
+            packageId = "AppInstallerTest.TestPortableExe";
+            packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
+            commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
 
-            // var result = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe");
-            // Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("Successfully installed"));
-            // // If no location specified, default behavior is to create a package directory with the name "{packageId}_{sourceId}"
-            // TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, true);
+            var searchResult = FindOnePackage(PackageMatchField.Id, PackageFieldMatchOption.Equals, packageId);
+            var installResult = await Install(searchResult.CatalogPackage, silent: false, randomDir: false);
+            Assert.True(InstallResultStatus.Ok == installResult.Status);
+
+            // If no location specified, default behavior is to create a package directory with the name "{packageId}_{sourceId}"
+            TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, true);
         }
 
         [Test]
-        public void InstallPortableExeWithCommand()
+        public async Task InstallPortableExeWithCommand()
         {
-            // var installDir = TestCommon.GetRandomTestDir();
-            // string packageId, commandAlias, fileName, productCode;
-            // packageId = "AppInstallerTest.TestPortableExeWithCommand";
-            //  productCode = packageId + "_" + Constants.TestSourceIdentifier;
-            // fileName = "AppInstallerTestExeInstaller.exe";
-            // commandAlias = "testCommand.exe";
+            string packageId, commandAlias, fileName, productCode;
+            packageId = "AppInstallerTest.TestPortableExeWithCommand";
+            productCode = packageId + "_" + Constants.TestSourceIdentifier;
+            fileName = "AppInstallerTestExeInstaller.exe";
+            commandAlias = "testCommand.exe";
 
-            // var result = TestCommon.RunAICLICommand("install", $"{packageId} -l {installDir}");
-            // Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("Successfully installed"));
-            // TestCommon.VerifyPortablePackage(installDir, commandAlias, fileName, productCode, true);
+            var searchResult = FindOnePackage(PackageMatchField.Id, PackageFieldMatchOption.Equals, packageId);
+            var installResult = await Install(searchResult.CatalogPackage, silent: false);
+            Assert.True(InstallResultStatus.Ok == installResult.Status);
+            TestCommon.VerifyPortablePackage(installDir, commandAlias, fileName, productCode, true);
         }
 
         [Test]
         public void InstallPortableExeWithRename()
         {
-            // var installDir = TestCommon.GetRandomTestDir();
-            // string packageId, productCode, renameArgValue;
-            // packageId = "AppInstallerTest.TestPortableExeWithCommand";
-            // productCode = packageId + "_" + Constants.TestSourceIdentifier;
-            // renameArgValue = "testRename.exe";
-
-            // var result = TestCommon.RunAICLICommand("install", $"{packageId} -l {installDir} --rename {renameArgValue}");
-            // Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("Successfully installed"));
-            // TestCommon.VerifyPortablePackage(installDir, renameArgValue, renameArgValue, productCode, true);
+            // TODO
+            Assert.Ignore();
         }
 
         [Test]
         public void InstallPortableInvalidRename()
         {
-            // var installDir = TestCommon.GetRandomTestDir();
-            // string packageId, renameArgValue;
-            // packageId = "AppInstallerTest.TestPortableExeWithCommand";
-            // renameArgValue = "test?";
-
-            // var result = TestCommon.RunAICLICommand("install", $"{packageId} -l {installDir} --rename {renameArgValue}");
-            // Assert.AreNotEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("The specified filename is not a valid filename"));
+            // TODO
+            Assert.Ignore();
         }
 
         [Test]
         public void InstallPortableReservedNames()
         {
-            // var installDir = TestCommon.GetRandomTestDir();
-            // string packageId, renameArgValue;
-            // packageId = "AppInstallerTest.TestPortableExeWithCommand";
-            // renameArgValue = "CON";
-
-            // var result = TestCommon.RunAICLICommand("install", $"{packageId} -l {installDir} --rename {renameArgValue}");
-            // Assert.AreNotEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("The specified filename is not a valid filename"));
+            // TODO
+            Assert.Ignore();
         }
 
         [Test]
-        public void InstallPortableToExistingDirectory()
+        public async Task InstallPortableToExistingDirectory()
         {
-            // var installDir = TestCommon.GetRandomTestDir();
-            // var existingDir = Path.Combine(installDir, "testDirectory");
-            // Directory.CreateDirectory(existingDir);
+            var existingDir = Path.Combine(installDir, "testDirectory");
+            Directory.CreateDirectory(existingDir);
 
-            // string packageId, commandAlias, fileName, productCode;
-            // packageId = "AppInstallerTest.TestPortableExe";
-            // productCode = packageId + "_" + Constants.TestSourceIdentifier;
-            // commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
+            string packageId, commandAlias, fileName, productCode;
+            packageId = "AppInstallerTest.TestPortableExe";
+            productCode = packageId + "_" + Constants.TestSourceIdentifier;
+            commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
 
-            // var result = TestCommon.RunAICLICommand("install", $"AppInstallerTest.TestPortableExe -l {existingDir}");
-            // Assert.AreEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("Successfully installed"));
-            // TestCommon.VerifyPortablePackage(existingDir, commandAlias, fileName, productCode, true);
+            var searchResult = FindOnePackage(PackageMatchField.Id, PackageFieldMatchOption.Equals, packageId);
+            installDir = existingDir;
+            var installResult = await Install(searchResult.CatalogPackage, silent: false);
+            Assert.True(InstallResultStatus.Ok == installResult.Status);
+            TestCommon.VerifyPortablePackage(existingDir, commandAlias, fileName, productCode, true);
         }
 
         [Test]
-        public void InstallPortableFailsWithCleanup()
+        public async Task InstallPortableFailsWithCleanup()
         {
-            // string winGetDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet");
-            // string installDir = Path.Combine(winGetDir, "Packages");
-            // string packageId, commandAlias, fileName, packageDirName, productCode;
-            // packageId = "AppInstallerTest.TestPortableExe";
-            // packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
-            // commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
+            // TODO: To follow up
+            Assert.Ignore();
 
-            // // Create a directory with the same name as the symlink in order to cause install to fail.
-            // string symlinkDirectory = Path.Combine(winGetDir, "Links");
-            // string conflictDirectory = Path.Combine(symlinkDirectory, commandAlias);
-            // Directory.CreateDirectory(conflictDirectory);
+            string winGetDir = Path.Combine(System.Environment.GetEnvironmentVariable("LocalAppData"), "Microsoft", "WinGet");
+            string installDir = Path.Combine(winGetDir, "Packages");
+            string packageId, commandAlias, fileName, packageDirName, productCode;
+            packageId = "AppInstallerTest.TestPortableExe";
+            packageDirName = productCode = packageId + "_" + Constants.TestSourceIdentifier;
+            commandAlias = fileName = "AppInstallerTestExeInstaller.exe";
 
-            // var result = TestCommon.RunAICLICommand("install", "AppInstallerTest.TestPortableExe");
+            // Create a directory with the same name as the symlink in order to cause install to fail.
+            string symlinkDirectory = Path.Combine(winGetDir, "Links");
+            string conflictDirectory = Path.Combine(symlinkDirectory, commandAlias);
+            Directory.CreateDirectory(conflictDirectory);
 
-            // // Remove directory prior to assertions as this will impact other tests if assertions fail.
-            // Directory.Delete(conflictDirectory, true);
+            var searchResult = FindOnePackage(PackageMatchField.Id, PackageFieldMatchOption.Equals, packageId);
+            var installResult = await Install(searchResult.CatalogPackage, silent: false, randomDir: false);
 
-            // Assert.AreNotEqual(Constants.ErrorCode.S_OK, result.ExitCode);
-            // Assert.True(result.StdOut.Contains("Unable to create symlink, path points to a directory."));
-            // TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, false);
+            // Remove directory prior to assertions as this will impact other tests if assertions fail.
+            Directory.Delete(conflictDirectory, true);
+
+            Assert.AreNotEqual(InstallResultStatus.Ok, installResult.Status);
+            TestCommon.VerifyPortablePackage(Path.Combine(installDir, packageDirName), commandAlias, fileName, productCode, false);
         }
 
-        public IReadOnlyList<MatchResult> FindAllPackages(PackageMatchField field, PackageFieldMatchOption option, string value)
+        public IReadOnlyList<MatchResult> FindAllPackages(
+            PackageMatchField field,
+            PackageFieldMatchOption option,
+            string value,
+            string source = TestPackageCatalog)
         {
-            FindPackagesOptions findPackageOptions = new FindPackagesOptions();
+            var findPackageOptions = new FindPackagesOptions();
             findPackageOptions.Filters.Add(new()
             {
                 Field = field,
@@ -295,17 +255,24 @@ namespace AppInstallerCLIE2ETests
                 Value = value
             });
 
+            var testSource = packageManager.GetPackageCatalogByName(source);
+            Assert.NotNull(testSource, $"Ensure that {source} is added");
+            var owcSource = testSource.Connect().PackageCatalog;
             return owcSource.FindPackages(findPackageOptions).Matches;
         }
 
-        public MatchResult FindOnePackage(PackageMatchField field, PackageFieldMatchOption option, string value)
+        public MatchResult FindOnePackage(
+            PackageMatchField field,
+            PackageFieldMatchOption option,
+            string value,
+            string source = TestPackageCatalog)
         {
-            var findPackages = FindAllPackages(field, option, value);
+            var findPackages = FindAllPackages(field, option, value, source);
             Assert.True(1 == findPackages.Count);
             return findPackages.First();
         }
 
-        public async Task<InstallResult> Install(CatalogPackage package, bool silent = true)
+        public async Task<InstallResult> Install(CatalogPackage package, bool silent = true, bool randomDir = true)
         {
             // Configure install options
             var installOptions = new InstallOptions();
@@ -313,7 +280,11 @@ namespace AppInstallerCLIE2ETests
             {
                 installOptions.PackageInstallMode = PackageInstallMode.Silent;
             }
-            installOptions.PreferredInstallLocation = installDir;
+
+            if (randomDir)
+            {
+                installOptions.PreferredInstallLocation = installDir;
+            }
 
             return await packageManager.InstallPackageAsync(package, installOptions);
         }
