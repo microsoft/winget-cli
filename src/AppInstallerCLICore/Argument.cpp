4c513ec57898b4c5e9b7eea4ab2cade5482932c6
Kaleb Luedtke <jluedtk@jci.com>
2022-07-05 14:08:24 -0500

    Switch order of Name/AlternateName

--- src/AppInstallerCLICore/Argument.cpp
+++ src/AppInstallerCLICore/Argument.cpp
@@ -118,11 +118,11 @@ namespace AppInstaller::CLI
         {
             strstr << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << m_alias << ',';
         }
-        strstr << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << m_name;
         if (m_alternateName != Argument::NoAlternateName)
         {
-            strstr << ',' << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << m_alternateName;
+            strstr << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << m_alternateName << ',';
         }
+        strstr << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << APPINSTALLER_CLI_ARGUMENT_IDENTIFIER_CHAR << m_name;
         return strstr.str();
     }
 
