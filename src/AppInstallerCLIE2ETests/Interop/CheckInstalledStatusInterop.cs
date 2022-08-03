f743f7d34d063f826885f0e8561ff8572a8148b2
Yao Sun <yaosun@microsoft.com>
2022-08-03 16:57:22 -0700

    test debug

--- src/AppInstallerCLIE2ETests/Interop/CheckInstalledStatusInterop.cs
+++ src/AppInstallerCLIE2ETests/Interop/CheckInstalledStatusInterop.cs
@@ -26,23 +26,48 @@ namespace AppInstallerCLIE2ETests.Interop
             packageManager = TestFactory.CreatePackageManager();
             testSource = packageManager.GetPackageCatalogByName(Constants.TestSourceName);
             installDir = TestCommon.GetRandomTestDir();
-        }
 
-        [Test]
-        public async Task CheckInstalledStatus()
-        {
             // Find and install the test package.
             var searchResult = FindOnePackage(testSource, PackageMatchField.Id, PackageFieldMatchOption.Equals, "AppInstallerTest.TestCheckInstalledStatus");
             var installOptions = TestFactory.CreateInstallOptions();
             installOptions.ReplacementInstallerArguments = $"/InstallDir {installDir} /ProductID CheckInstalledStatusProductId /DisplayName TestCheckInstalledStatus";
-            var installResult = await packageManager.InstallPackageAsync(searchResult.CatalogPackage, installOptions);
+            var installResult = packageManager.InstallPackageAsync(searchResult.CatalogPackage, installOptions).GetResults();
             Assert.AreEqual(InstallResultStatus.Ok, installResult.Status);
 
+            // Add the data file listed in the manifest
+            File.WriteAllText(Path.Combine(installDir, "data.txt"), "Test");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            // Find and uninstall the test package.
+            var options = TestFactory.CreateCreateCompositePackageCatalogOptions();
+            options.Catalogs.Add(testSource);
+            options.CompositeSearchBehavior = CompositeSearchBehavior.AllCatalogs;
+            var compositeSource = packageManager.CreateCompositePackageCatalog(options);
+            var searchResult = FindOnePackage(compositeSource, PackageMatchField.Id, PackageFieldMatchOption.Equals, "AppInstallerTest.TestCheckInstalledStatus");
+            var uninstallOptions = TestFactory.CreateUninstallOptions();
+            var uninstallResult = packageManager.UninstallPackageAsync(searchResult.CatalogPackage, uninstallOptions).GetResults();
+            Assert.AreEqual(UninstallResultStatus.Ok, uninstallResult.Status);
+
+            // Delete the extra data file listed in the manifest
+            File.Delete(Path.Combine(installDir, "data.txt"));
+        }
+
+        [Test]
+        public void CheckInstalledStatus()
+        {
+            var searchResult = FindOnePackage(testSource, PackageMatchField.Id, PackageFieldMatchOption.Equals, "AppInstallerTest.TestCheckInstalledStatus");
             var checkResult = searchResult.CatalogPackage.CheckInstalledStatus();
             Assert.AreEqual(CheckInstalledStatusResultStatus.Ok, checkResult.Status);
             Assert.AreEqual(1, checkResult.PackageInstalledStatus.Count);
 
-            // checkResult.InstalledStatus[0].
+            var installerInstalledStatus = checkResult.PackageInstalledStatus[0];
+            Assert.AreEqual(Windows.System.ProcessorArchitecture.Neutral, installerInstalledStatus.InstallerInfo.Architecture);
+            Assert.AreEqual(PackageInstallerType.Exe, installerInstalledStatus.InstallerInfo.InstallerType);
+            Assert.AreEqual(PackageInstallerType.Unknown, installerInstalledStatus.InstallerInfo.NestedInstallerType);
+            Assert.AreEqual(PackageInstallerScope.Unknown, installerInstalledStatus.InstallerInfo.Scope);
         }
     }
 }
