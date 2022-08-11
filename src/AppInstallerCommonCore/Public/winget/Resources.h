b1af99306f103d8859091a3d3818d5ac8fd0b796
Amir El Bawab <104940545+AmelBawa-msft@users.noreply.github.com>
2022-08-11 10:59:15 -0700

    Moved Loc to common resources

--- src/AppInstallerCommonCore/Public/winget/Resources.h
+++ src/AppInstallerCommonCore/Public/winget/Resources.h
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <vector>
+#include "LocIndependent.h"
 
 using namespace std::string_view_literals;
 
@@ -55,5 +56,45 @@ namespace AppInstaller
         // Resource data is valid as long as the binary is loaded.
         std::pair<const BYTE*, size_t> GetResourceAsBytes(int resourceName, int resourceType);
         std::pair<const BYTE*, size_t> GetResourceAsBytes(PCWSTR resourceName, PCWSTR resourceType);
+
+        // A localized string
+        struct LocString : public Utility::LocIndString
+        {
+            LocString() = default;
+
+            LocString(StringResource::StringId id);
+
+            LocString(const LocString&) = default;
+            LocString& operator=(const LocString&) = default;
+
+            LocString(LocString&&) = default;
+            LocString& operator=(LocString&&) = default;
+        };
+
+        // Utility class to load resources
+        class Loader
+        {
+        public:
+            // Gets the singleton instance of the resource loader.
+            static const Loader& Instance();
+
+            // Gets the the string resource value.
+            std::string ResolveString(std::wstring_view resKey) const;
+
+        private:
+            winrt::Windows::ApplicationModel::Resources::ResourceLoader m_wingetLoader;
+
+            Loader();
+        };
+
+        struct ResourceOpenException : std::exception
+        {
+            ResourceOpenException(const winrt::hresult_error& hre);
+
+            const char* what() const noexcept override { return m_message.c_str(); }
+
+        private:
+            std::string m_message;
+        };
     }
 }
--- src/AppInstallerCommonCore/Resources.cpp
+++ src/AppInstallerCommonCore/Resources.cpp
@@ -4,6 +4,7 @@
 #include "winget/Resources.h"
 #include "Public/AppInstallerLogging.h"
 #include "Public/AppInstallerStrings.h"
+#include "Public//AppInstallerErrors.h"
 
 namespace AppInstaller::Resource
 {
@@ -54,5 +55,45 @@ namespace AppInstaller::Resource
     {
         auto resourceData = GetResourceData(resourceName, resourceType);
         return std::make_pair(reinterpret_cast<BYTE*>(resourceData.first), resourceData.second);
+    }
+
+    LocString::LocString(StringResource::StringId id) : Utility::LocIndString(Loader::Instance().ResolveString(id)) {}
+
+    const Loader& Loader::Instance()
+    {
+        static Loader instance;
+        return instance;
+    }
+
+    Loader::Loader() : m_wingetLoader(nullptr)
+    {
+        try
+        {
+            // The default constructor of ResourceLoader throws a winrt::hresult_error exception
+            // when resource.pri is not found. ResourceLoader::GetForViewIndependentUse also throws
+            // a winrt::hresult_error but for reasons unknown it only gets catch when running on the
+            // debugger. Running without a debugger will result in a crash that not even adding a
+            // catch all fix. To provide a good error message we call the default constructor
+            // before calling GetForViewIndependentUse.
+            m_wingetLoader = winrt::Windows::ApplicationModel::Resources::ResourceLoader();
+            m_wingetLoader = winrt::Windows::ApplicationModel::Resources::ResourceLoader::GetForViewIndependentUse(L"winget");
+        }
+        catch (const winrt::hresult_error& hre)
+        {
+            // This message cannot be localized.
+            AICLI_LOG(CLI, Error, << "Failure loading resource file with error: " << hre.code());
+            throw ResourceOpenException(hre);
+        }
+    }
+
+    std::string Loader::ResolveString(
+        std::wstring_view resKey) const
+    {
+        return Utility::ConvertToUTF8(m_wingetLoader.GetString(resKey));
+    }
+
+    ResourceOpenException::ResourceOpenException(const winrt::hresult_error& hre)
+    {
+        m_message = "Could not open the resource file: " + GetUserPresentableMessage(hre);
     }
 }
