993de6b6996023c399071be59fdf35ba60cd1d83
Akinwale Alagbe <24412729+hackean-msft@users.noreply.github.com>
2022-09-13 11:41:12 -0700

    Return error codes for dependencies failure (#2410)
    * Surface dependencies specific errors
    
    * Updated flags result
    
    * Sort alphabetically
    
    * Return message ID when string mapping not found
    
    * Addressed PR comments
    
    * Renamed field to context
    
    * Proper name change
    
    * Addressed PR comments
    
    * Addressed PR comments
    
    * Change method name to reflect functionality.
    
    * Included excluded files.
    
    * Addressed PR comments
    
    * Spaces and indentation fix.
    
    * Add capability to return multiple error result.
    
    * code refactor
    
    * Addressed pr comment
    
    * Addressed PR comments
    
    * Addressed pr comment
    
    Co-authored-by: Akinwale Alagbe <akalagbe@microsoft.com>

--- src/AppInstallerCommonCore/Manifest/ManifestSchemaValidation.cpp
+++ src/AppInstallerCommonCore/Manifest/ManifestSchemaValidation.cpp
@@ -187,7 +187,7 @@ namespace AppInstaller::Manifest::YamlParser
 
             if (!JsonSchema::Validate(schema, manifestJson, results))
             {
-                errors.emplace_back(ValidationError::MessageWithFile(JsonSchema::GetErrorStringFromResults(results), entry.FileName));
+                errors.emplace_back(ValidationError::MessageContextWithFile(ManifestError::SchemaError, JsonSchema::GetErrorStringFromResults(results), entry.FileName));
             }
         }
 
