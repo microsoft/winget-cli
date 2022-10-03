97f40d5c515848eafcf399e3772df6fd1d2fe0f3
Ryan Fu <Fu.Ryan@microsoft.com>
2022-10-03 12:24:03 -0700

    try again

--- src/AppInstallerCommonCore/Archive.cpp
+++ src/AppInstallerCommonCore/Archive.cpp
@@ -4,6 +4,8 @@
 #include "winget/Archive.h"
 #pragma warning( push )
 #pragma warning ( disable : 4189 )
+#pragma warning ( disable : 4244 )
+#pragma warning ( disable : 26451 )
 #include "pure.h"
 #pragma warning ( pop )
 
