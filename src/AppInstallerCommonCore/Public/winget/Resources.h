c827f20c0441e07d33de0fe7bcec11e07bb2efd2
yao-msft <50888816+yao-msft@users.noreply.github.com>
2022-12-14 14:14:37 -0800

    Update Resources.h

--- src/AppInstallerCommonCore/Public/winget/Resources.h
+++ src/AppInstallerCommonCore/Public/winget/Resources.h
@@ -31,7 +31,6 @@ namespace AppInstaller
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyEnableLocalManifests);
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyEnableHashOverride);
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyEnableLocalArchiveMalwareScanOverride);
-
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyEnableDefaultSource);
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyEnableMSStoreSource);
             WINGET_DEFINE_RESOURCE_STRINGID(PolicyAdditionalSources);
