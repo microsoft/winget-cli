99c17146caaa6a46fd62bf40ad11cb9fe49dc5e5
JohnMcPMS <johnmcp@microsoft.com>
2022-12-15 11:45:56 -0800

    Commit before moving to internal

---
+++ src/Microsoft.Management.Configuration/pch.h
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+#include <unknwn.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+
+#include <mutex>
