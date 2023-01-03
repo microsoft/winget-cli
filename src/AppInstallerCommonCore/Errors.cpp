4af7d5d4da040ad0ee300beaf21580bd4ad397f1
Flor Chacón <14323496+florelis@users.noreply.github.com>
2023-01-03 13:44:15 -0800

    Update src/AppInstallerCommonCore/Errors.cpp
    Co-authored-by: Kaleb Luedtke <trenlymc@gmail.com>

--- src/AppInstallerCommonCore/Errors.cpp
+++ src/AppInstallerCommonCore/Errors.cpp
@@ -213,7 +213,7 @@ namespace AppInstaller
             case APPINSTALLER_CLI_ERROR_PIN_DOES_NOT_EXIST:
                 return "There is no pin for the package.";
             case APPINSTALLER_CLI_ERROR_PACKAGE_HAS_BLOCKING_PIN:
-                return "The package has a blocking pin that prevents from updating.";
+                return "The package has a blocking pin that prevents upgrade.";
 
             // Install errors
             case APPINSTALLER_CLI_ERROR_INSTALL_PACKAGE_IN_USE:
