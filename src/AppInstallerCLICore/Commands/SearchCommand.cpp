36d4495115c5cb1b112b2f6d48440eca2e9133ea
Kaleb Luedtke <jluedtk@jci.com>
2023-01-24 22:01:15 -0600

    Allow Version Listing through 'Winget Search' (#2847)

--- src/AppInstallerCLICore/Commands/SearchCommand.cpp
+++ src/AppInstallerCLICore/Commands/SearchCommand.cpp
@@ -25,6 +25,7 @@ namespace AppInstaller::CLI
             Argument::ForType(Execution::Args::Type::Exact),
             Argument::ForType(Execution::Args::Type::CustomHeader),
             Argument::ForType(Execution::Args::Type::AcceptSourceAgreements),
+            Argument::ForType(Execution::Args::Type::ListVersions),
         };
     }
 
@@ -78,8 +79,21 @@ namespace AppInstaller::CLI
         context <<
             Workflow::OpenSource() <<
             Workflow::SearchSourceForMany <<
-            Workflow::HandleSearchResultFailures <<
-            Workflow::EnsureMatchesFromSearchResult(false) <<
-            Workflow::ReportSearchResult;
+            Workflow::HandleSearchResultFailures;
+
+            if (context.Args.Contains(Execution::Args::Type::ListVersions))
+            {
+                context <<
+                Workflow::EnsureOneMatchFromSearchResult(false) <<
+                Workflow::ReportPackageIdentity <<
+                Workflow::ShowAppVersions;
+            }
+            else
+            {
+                context << 
+                    Workflow::EnsureMatchesFromSearchResult(false) <<
+                    Workflow::ReportSearchResult;
+            }
+        
     }
 }
