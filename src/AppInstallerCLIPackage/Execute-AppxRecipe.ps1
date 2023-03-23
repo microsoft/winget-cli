557f42a002a0a4d27682fc65a997a6958aef1587
JohnMcPMS <johnmcp@microsoft.com>
2023-03-23 11:43:05 -0700

    Add powershell script to execute the recipe and change pipelines to use it

--- src/AppInstallerCLIPackage/Execute-AppxRecipe.ps1
+++ src/AppInstallerCLIPackage/Execute-AppxRecipe.ps1
@@ -1,9 +1,13 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true,Position=0)]
     [string]$AppxRecipePath,
 
-    [string]$LayoutPath
+    [string]$LayoutPath,
+
+    [switch]$Force
 )
 
 [xml]$Local:recipe = Get-Content $AppxRecipePath
@@ -22,6 +26,25 @@ $Local:packageFileElements = Select-Xml -Xml $Local:recipe -Namespace $Local:nam
 
 $Local:allItems = $Local:packageFileElements + $Local:manifestElement
 
-ForEach-Object -InputObject $Local:allItems -Process {
-    $_.Include
+$Local:progressActivity = "Copying files to $LayoutPath"
+Write-Progress -Activity $Local:progressActivity
+
+if ($Force) {
+    $null = New-Item -Path $LayoutPath -ItemType Directory -Force
+} else {
+    New-Item -Path $LayoutPath -ItemType Directory -ErrorAction Inquire
 }
+
+[Int32]$Local:filesCopied = 0
+
+$Local:allItems | ForEach-Object -Process {
+    $Local:sourcePath = $_.Include
+    $Local:destinationPath = Join-Path $LayoutPath $_.PackagePath
+    Write-Verbose "$Local:sourcePath => $Local:destinationPath"
+    $null = New-Item -Path ([System.IO.Path]::GetDirectoryName($Local:destinationPath)) -ItemType Directory -Force
+    Copy-Item -Path $Local:sourcePath -Destination $Local:destinationPath
+    $Local:filesCopied += 1
+    Write-Progress -Activity $Local:progressActivity -PercentComplete (($Local:filesCopied * 100) / $Local:allItems.Count)
+}
+
+Write-Progress -Activity $Local:progressActivity -Completed
