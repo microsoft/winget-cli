9580335f144eb682efbb7d184d1ac6f31dc1f78e
Ruben Guerrero Samaniego <rubengu@microsoft.com>
2023-04-18 23:14:23 -0700

    remove commented code

--- src/PowerShell/Microsoft.WinGet.Client.Cmdlets/Acl/ModuleInit.cs
+++ src/PowerShell/Microsoft.WinGet.Client.Cmdlets/Acl/ModuleInit.cs
@@ -18,14 +18,12 @@ namespace Microsoft.WinGet.Client.Acl
         public void OnImport()
         {
             AssemblyLoadContext.Default.Resolving += WinGetAssemblyLoadContext.ResolvingHandler;
-            //AssemblyLoadContext.Default.ResolvingUnmanagedDll += WinGetAssemblyLoadContext.ResolvingUnmanagedDllHandler;
         }
 
         /// <inheritdoc/>
         public void OnRemove(PSModuleInfo module)
         {
             AssemblyLoadContext.Default.Resolving -= WinGetAssemblyLoadContext.ResolvingHandler;
-            //AssemblyLoadContext.Default.ResolvingUnmanagedDll -= WinGetAssemblyLoadContext.ResolvingUnmanagedDllHandler;
         }
     }
 }
