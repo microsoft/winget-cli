4aab7f7e9826af135807ea965167b8990c90a7a9
Ruben Guerrero <rubengu@microsoft.com>
2023-04-20 09:57:42 -0700

    Microsoft.WinGet.Client custom assembly load context (#3150)
    This PR modifies the Microsoft.WinGet.Client module to be more dependency friendly and play nice with winget configure.
    
    It uses a custom AssemblyLoadContext that handles loading all the assembly dependencies of this module. For this, the Microsoft.WinGet.Client.Engine project was created. This project wraps all the cmdlet functionality on it and leaves the Microsoft.WinGet.Client.Cmdlets.dll (formerly Microsoft.WinGet.Client.dll) to just define the visibility of the cmdlets. The custom ALC is registered at onImport module initializer and cleaned up when the module is removed.
    
    To avoid loading any other dlls at import module time, none of the cswinrt objects from Microsoft.Management.Deployment are exposed as parameters or outputs of the cmdlets. The engine contains PS wrappers objects and enum that are converted internally by and only by the engine.
    
    Other fixes:
    
    Removed the dependency on the PowerShellSDK for the PowerShell 7 version of the binary. It was used to call an API to convert json into hashtable but this wouldn't work for Windows PowerShell. Instead, I just took what PowerShell 7 does and applied it to both. We still don't support Windows PowerShell, but is a step forward into doing it.
    
    Fixed passing a catalog package object back to the cmdlets to avoid the search. It got broken when Find-WinGetPackage and Get-WinGetPackage started returning an object instead of the cswinrt CatalogPackage object. The wrapper contains the catalog package but is not accessible to the user, so there was no way to pass it as a parameter back to Install-WinGetPackage,
    
    Modified the PowerShell output directory for binary to follow more what nuget suggests for platform specific binaries.

---
+++ src/PowerShell/Microsoft.WinGet.Client.Engine/Commands/UninstallPackageCommand.cs
@@ -0,0 +1,139 @@
+﻿// -----------------------------------------------------------------------------
+// <copyright file="UninstallPackageCommand.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Microsoft.WinGet.Client.Engine.Commands
+{
+    using System;
+    using System.Management.Automation;
+    using Microsoft.Management.Deployment;
+    using Microsoft.WinGet.Client.Engine.Commands.Common;
+    using Microsoft.WinGet.Client.Engine.Extensions;
+    using Microsoft.WinGet.Client.Engine.Helpers;
+    using Microsoft.WinGet.Client.Engine.Properties;
+    using Microsoft.WinGet.Client.Engine.PSObjects;
+
+    /// <summary>
+    /// Uninstalls a package from the local system.
+    /// </summary>
+    public sealed class UninstallPackageCommand : PackageCommand
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UninstallPackageCommand"/> class.
+        /// </summary>
+        /// <param name="psCmdlet">Caller cmdlet.</param>
+        /// <param name="psCatalogPackage">PSCatalogPackage.</param>
+        /// <param name="version">Version to install.</param>
+        /// <param name="log">Logging file location.</param>
+        /// <param name="id">Package identifier.</param>
+        /// <param name="name">Name of package.</param>
+        /// <param name="moniker">Moniker of package.</param>
+        /// <param name="source">Source to search. If null, all are searched.</param>
+        /// <param name="query">Match against any field of a package.</param>
+        /// <param name="exact">Do exact match.</param>
+        public UninstallPackageCommand(
+            PSCmdlet psCmdlet,
+            PSCatalogPackage psCatalogPackage,
+            string version,
+            string log,
+            string id,
+            string name,
+            string moniker,
+            string source,
+            string[] query,
+            bool exact)
+            : base(psCmdlet)
+        {
+            // PackageCommand.
+            if (psCatalogPackage != null)
+            {
+                this.CatalogPackage = psCatalogPackage.CatalogPackageCOM;
+            }
+
+            this.Version = version;
+            this.Log = log;
+
+            // FinderCommand
+            this.Id = id;
+            this.Name = name;
+            this.Moniker = moniker;
+            this.Source = source;
+            this.Query = query;
+            this.Exact = exact;
+        }
+
+        /// <summary>
+        /// Process uninstall package.
+        /// </summary>
+        /// <param name="psPackageUninstallMode">PSPackageUninstallMode.</param>
+        /// <param name="force">Force.</param>
+        public void Uninstall(
+            string psPackageUninstallMode,
+            bool force)
+        {
+            this.GetPackageAndExecute(CompositeSearchBehavior.LocalCatalogs, (package, version) =>
+            {
+                UninstallOptions options = this.GetUninstallOptions(version, PSEnumHelpers.ToPackageUninstallMode(psPackageUninstallMode), force);
+                UninstallResult result = this.UninstallPackage(package, options);
+                this.PsCmdlet.WriteObject(new PSUninstallResult(result));
+            });
+        }
+
+        private UninstallOptions GetUninstallOptions(
+            PackageVersionId version,
+            PackageUninstallMode packageUninstallMode,
+            bool force)
+        {
+            var options = ComObjectFactory.Value.CreateUninstallOptions();
+            options.Force = force;
+            if (this.Log != null)
+            {
+                options.LogOutputPath = this.Log;
+            }
+
+            options.PackageUninstallMode = packageUninstallMode;
+
+            if (version != null)
+            {
+                options.PackageVersionId = version;
+            }
+
+            return options;
+        }
+
+        private UninstallResult UninstallPackage(
+            CatalogPackage package,
+            UninstallOptions options)
+        {
+            string activity = string.Format(
+                Resources.ProgressRecordActivityUninstalling,
+                package.Name);
+
+            var operation = PackageManager.Value.UninstallPackageAsync(package, options);
+            WriteProgressAdapter adapter = new (this.PsCmdlet);
+            operation.Progress = (context, progress) =>
+            {
+                adapter.WriteProgress(new ProgressRecord(1, activity, progress.State.ToString())
+                {
+                    RecordType = ProgressRecordType.Processing,
+                });
+            };
+            operation.Completed = (context, status) =>
+            {
+                adapter.WriteProgress(new ProgressRecord(1, activity, status.ToString())
+                {
+                    RecordType = ProgressRecordType.Completed,
+                });
+                adapter.Completed = true;
+            };
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                operation.Cancel();
+            };
+            adapter.Wait();
+            return operation.GetResults();
+        }
+    }
+}
