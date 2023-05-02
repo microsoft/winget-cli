4b7b3e093c0f79e061a5f7bd787588e1fed71602
JohnMcPMS <johnmcp@microsoft.com>
2023-05-02 16:37:40 -0700

    Move to main solution for PS build, update a few more projects with manual target frameworks, add CFG setting for configuration

---
+++ src/PowerShell/Microsoft.WinGet.Client.Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+using System.Runtime.Versioning;
+
+#if NET
+
+// Forcibly set the target and supported platforms due to the internal build setup.
+// Keep in sync with project versions.
+[assembly: TargetPlatform("Windows10.0.22000.0")]
+[assembly: SupportedOSPlatform("Windows10.0.18362.0")]
+
+#endif
