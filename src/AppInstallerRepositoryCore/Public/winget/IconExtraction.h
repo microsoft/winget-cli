5ff068c0a40c0e0641cc458eafe993183e6e1f90
Yao Sun <yaosun@microsoft.com>
2023-05-04 14:52:28 -0700

    improve

--- src/AppInstallerRepositoryCore/Public/winget/IconExtraction.h
+++ src/AppInstallerRepositoryCore/Public/winget/IconExtraction.h
@@ -20,11 +20,11 @@ namespace AppInstaller::Repository
     // The icon index usage is basically:
     //     0, 1, 2        means zero-based index of the first icon to extract.
     //     -1, -2, -3     means extract icon whose resource name is 1, 2, 3 etc
-    std::vector<BYTE> ExtractIconFromBinaryFile(const std::filesystem::path binaryPath, int iconIndex);
+    std::vector<BYTE> ExtractIconFromBinaryFile(const std::filesystem::path binaryPath, int iconIndex = 0);
 
     // Extracts the app icon given the app's product code.
     // This method uses simillar logic for retrieving icon as app list in settings page.
     // This method returns empty if only default icons would be picked.
     // This method returns contents of .ico icons.
-    std::vector<ExtractedIconInfo> ExtractIconFromArpEntry(const std::string& productCode, AppInstaller::Manifest::ScopeEnum scope);
+    std::vector<ExtractedIconInfo> ExtractIconFromArpEntry(const std::string& productCode, AppInstaller::Manifest::ScopeEnum scope = AppInstaller::Manifest::ScopeEnum::Unknown);
 }
