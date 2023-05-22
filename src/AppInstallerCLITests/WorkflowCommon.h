dc8ee6bb5476cc7bcb1ee32c2db3ef98b215af8a
Flor Elisa Chacon Ochoa <florch@microsoft.com>
2023-05-22 16:20:48 -0700

    Some more errors from static analysis

--- src/AppInstallerCLITests/WorkflowCommon.h
+++ src/AppInstallerCLITests/WorkflowCommon.h
@@ -11,10 +11,7 @@
 
 #define REQUIRE_TERMINATED_WITH(_context_,_hr_) \
     REQUIRE(_context_.IsTerminated()); \
-    REQUIRE(( (_hr_) == (_context_).GetTerminationHR() ))
-
-#define REQUIRE_SUCCEEDED(_context_) \
-    REQUIRE(( S_OK == (_context_).GetTerminationHR() ))
+    REQUIRE(static_cast<HRESULT>(_hr_) == _context_.GetTerminationHR())
 
 namespace TestCommon
 {
@@ -38,6 +35,7 @@ namespace TestCommon
         const extern TestSourceResult TestInstaller_Exe;
         const extern TestSourceResult TestInstaller_Exe_Dependencies;
         const extern TestSourceResult TestInstaller_Exe_DifferentInstallerType;
+        const extern TestSourceResult TestInstaller_Exe_ExpectedReturnCodes;
         const extern TestSourceResult TestInstaller_Exe_IncompatibleInstallerType;
         const extern TestSourceResult TestInstaller_Exe_LatestInstalled;
         const extern TestSourceResult TestInstaller_Exe_LicenseAgreement;
@@ -136,4 +134,11 @@ namespace TestCommon
 
     void OverrideForMSStore(TestContext& context, bool isUpdate);
 
+    void OverrideOpenDependencySource(TestContext& context);
+
+    void OverrideEnableWindowsFeaturesDependencies(TestContext& context);
+
+    void OverrideRegisterStartupAfterReboot(TestContext& context);
+
+    void OverrideDownloadInstallerFileForMSStoreDownload(TestContext& context);
 }
