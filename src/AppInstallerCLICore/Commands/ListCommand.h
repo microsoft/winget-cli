04403d434c44564e64aacb518d2d7d4fab80851f
Flor Chacón <14323496+florelis@users.noreply.github.com>
2023-07-31 12:27:53 -0700

    Allow `--include-unknown` in list `--upgrade-available` (#3473)

--- src/AppInstallerCLICore/Commands/ListCommand.h
+++ src/AppInstallerCLICore/Commands/ListCommand.h
@@ -20,6 +20,7 @@ namespace AppInstaller::CLI
         Utility::LocIndView HelpLink() const override;
 
     protected:
+        void ValidateArgumentsInternal(Execution::Args& execArgs) const override;
         void ExecuteInternal(Execution::Context& context) const override;
     };
 }
