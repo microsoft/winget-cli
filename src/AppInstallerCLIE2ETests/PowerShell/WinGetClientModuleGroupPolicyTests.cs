3ce797230ce37dcb86e988ccc091a7d4d73547f7
Madhusudhan Gumbalapura Sudarshan <Madhusudhan.Sudarshan@microsoft.com>
2023-08-15 12:12:26 -0700

    PowerShell modules i.e WinGetCLI cmdlet & WinGet Configure cmdlet to read and respect "EnableAppInstaller" & EnableWindowsPackageManagerCommandLineInterfaces Group Policies
    The commit includes:
    1. Shared code to read Group Policies "Microsoft.WinGet.SharedLib" right now limited to read only Toggle Policies
       just enough to address the scope of Intune deliverable.
    2. Group policy read and validation logic to block PowerShell modules
       1. Microsoft.WinGet.Client.psd1 PowerShell module
       2. Microsoft.WinGet.Configuration.psd1 PowerShell module
       when any of two policies are disabled
       "EnableAppInstaller" or "EnableWindowsPackageManagerCommandLineInterfaces"
    3. The PowerShell module consumes the "Microsoft.WinGet.SharedLib" share code as file links added to
        - Microsoft.WinGet.Client.Engine
        - Microsoft.WinGet.Configuration.Engine
        cs projects and they compiled into its own assemblies to address following issues
        1. PowerShell modules for WinGetClient & WinGet Configure target different OS versions
           & .net framework combination as a result having a common shared .net standard library
           fails to compile due to OS target version compatibility issues.
        2. We can also avoid duplicating same share code b/w PowerShell module projects.
    
        NOTE: GroupPolicyResource.resx resource is duplicated into PowerShell modules assemblies
        as adding link resulted runtime failure due to missing resource file
    4. Tests to validate PowerShell WinGet Client command with Group Policy in effect
    
    TODOs: Add Group Policy tests for WinGet Configure PowerShell cmdlets this is manually validated
    by running PowerShell cmdlets but actual test requires some additional setup so plan is add those
    test in future.

---
+++ src/AppInstallerCLIE2ETests/PowerShell/WinGetClientModuleGroupPolicyTests.cs
@@ -0,0 +1,93 @@
+﻿// -----------------------------------------------------------------------------
+// <copyright file="WinGetClientModuleGroupPolicyTests.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+namespace AppInstallerCLIE2ETests.PowerShell
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO.Packaging;
+    using System.Linq;
+    using System.Management.Automation.Runspaces;
+    using System.Text.RegularExpressions;
+    using AppInstallerCLIE2ETests.Helpers;
+    using Markdig.Extensions.Tables;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Basic E2E smoke tests for verifying the Group Policy behavior for the PowerShell Microsoft.WinGet.Client module cmdlets.
+    /// Running the x86 PowerShell Module requires PowerShell Core (x86). These tests currently only target PowerShell Core (x64)
+    /// in the CI/CD pipeline.
+    /// </summary>
+    [Category("PowerShell")]
+    public class WinGetClientModuleGroupPolicyTests
+    {
+        /// <summary>
+        /// Set setup.
+        /// </summary>
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            TestCommon.RunAICLICommand("source add", $"-n {Constants.TestSourceName} {Constants.TestSourceUrl}");
+        }
+
+        /// <summary>
+        /// Tear down.
+        /// </summary>
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            TestCommon.RunAICLICommand("source remove", $"{Constants.TestSourceName}");
+            WinGetSettingsHelper.InitializeWingetSettings();
+        }
+
+        /// <summary>
+        /// Set up.
+        /// </summary>
+        [SetUp]
+        public void TestMethodSetup()
+        {
+            GroupPolicyHelper.DeleteExistingPolicies();
+        }
+
+        /// <summary>
+        /// Tear down.
+        /// </summary>
+        [TearDown]
+        public void TestMethodTearDown()
+        {
+            GroupPolicyHelper.DeleteExistingPolicies();
+        }
+
+        /// <summary>
+        /// Validates WinGet Policy Disabled.
+        /// </summary>
+        [Test]
+        public void DisableWinGetPolicy()
+        {
+            GroupPolicyHelper.EnableWinget.Disable();
+
+            var result = TestCommon.RunPowerShellCoreCommandWithResult(Constants.GetSourceCmdlet, $"-Name {Constants.TestSourceName}");
+            Assert.AreNotEqual(Constants.ErrorCode.S_OK, result.ExitCode, $"ExitCode: {result.ExitCode} exited with success status: {result.StdOut}, {result.StdErr}");
+            Assert.IsNotNull(result.StdErr);
+            Assert.IsTrue(result.StdErr.Contains("This operation is disabled by Group Policy : Enable Windows Package Manager"));
+        }
+
+        /// <summary>
+        /// Validates EnableWinGetCLIInterfaces Policy Disabled.
+        /// </summary>
+        [Test]
+        public void EnableWinGetCLIInterfacesPolicy()
+        {
+            GroupPolicyHelper.EnableWinget.Enable();
+            GroupPolicyHelper.EnableWinGetCommandLineIntefaces.Disable();
+
+            var result = TestCommon.RunPowerShellCoreCommandWithResult(Constants.GetSourceCmdlet, $"-Name {Constants.TestSourceName}");
+            Assert.AreNotEqual(Constants.ErrorCode.S_OK, result.ExitCode, $"ExitCode: {result.ExitCode} exited with success status: {result.StdOut}, {result.StdErr}");
+            Assert.IsNotNull(result.StdErr);
+            Assert.IsTrue(result.StdErr.Contains("This operation is disabled by Group Policy : Enable Windows Package Manager command line interfaces"));
+        }
+    }
+}
