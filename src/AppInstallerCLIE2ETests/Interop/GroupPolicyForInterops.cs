f7be724fa5eefbba1f7d64cc23ece88810afa462
Madhusudhan Gumbalapura Sudarshan <Madhusudhan.Sudarshan@microsoft.com>
2023-08-18 17:44:09 -0700

    Guard WinRT InProc Window Package Manager Deployment APIs by EnableAppInstaller GroupPolicy
    [WHY:]
    WINRT InProcess Deployment APIs are current not guarded by EnableAppInstaller GroupPolicy.
    where as COM InProcess/OutProcess API are guarded by group policy.
    
    [WHAT:]
    - This fix is introducing the Group Policy check for WinRT InProcess deployment APIs
    to block them if EnableAppInstaller is disabled.
    - Added some group policy tests for the InProcess/ OutOfProcess Interops to validate
      group policy indeed blocks the Object creation when policy is disabled.
    
    [How Tested:]
    - Copied the fix and applied wingetdev package
    - GroupPolicyForInterops test run to ensure they all behave as expected.

---
+++ src/AppInstallerCLIE2ETests/Interop/GroupPolicyForInterops.cs
@@ -0,0 +1,85 @@
+﻿// -----------------------------------------------------------------------------
+// <copyright file="GroupPolicyForInterops.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
+// </copyright>
+// -----------------------------------------------------------------------------
+
+namespace AppInstallerCLIE2ETests.Interop
+{
+    using System.Runtime.InteropServices;
+    using Microsoft.Management.Deployment;
+    using Microsoft.Management.Deployment.Projection;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Group Policy Tests for Interops
+    /// </summary>
+    [TestFixtureSource(typeof(InstanceInitializersSource), nameof(InstanceInitializersSource.InProcess), Category = nameof(InstanceInitializersSource.InProcess))]
+    [TestFixtureSource(typeof(InstanceInitializersSource), nameof(InstanceInitializersSource.OutOfProcess), Category = nameof(InstanceInitializersSource.OutOfProcess))]
+    public class GroupPolicyForInterops : BaseInterop
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GroupPolicyForInterops"/> class.
+        /// </summary>
+        /// <param name="initializer">Initializer.</param>
+        public GroupPolicyForInterops(IInstanceInitializer initializer)
+          : base(initializer)
+        {
+        }
+
+        /// <summary>
+        /// Test setup.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            GroupPolicyHelper.DeleteExistingPolicies();
+        }
+
+        /// <summary>
+        /// Clean up.
+        /// </summary>
+        [TearDown]
+        public void CleanUp()
+        {
+            GroupPolicyHelper.DeleteExistingPolicies();
+        }
+
+        /// <summary>
+        /// Validates disabling WinGetPolicy should block COM/WinRT Objects creation (InProcess and OutofProcess).
+        /// </summary>
+        [Test]
+        public void DisableWinGetPolicy()
+        {
+            GroupPolicyHelper.EnableWinget.Disable();
+
+            COMException comException = Assert.Catch<COMException>(() => { PackageManager packageManager = this.TestFactory.CreatePackageManager(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { FindPackagesOptions findPackagesOptions = this.TestFactory.CreateFindPackagesOptions(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { CreateCompositePackageCatalogOptions createCompositePackageCatalogOptions = this.TestFactory.CreateCreateCompositePackageCatalogOptions(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { InstallOptions installOptions = this.TestFactory.CreateInstallOptions(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { UninstallOptions uninstallOptions = this.TestFactory.CreateUninstallOptions(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { DownloadOptions downloadOptions = this.TestFactory.CreateDownloadOptions(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            comException = Assert.Catch<COMException>(() => { PackageMatchFilter packageMatchFilter = this.TestFactory.CreatePackageMatchFilter(); });
+            Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+
+            // PackageManagerSettings is not implemented in context OutOfProcDev
+            if (this.TestFactory.Context == ClsidContext.InProc)
+            {
+                comException = Assert.Catch<COMException>(() => { PackageManagerSettings packageManagerSettings = this.TestFactory.CreatePackageManagerSettings(); });
+                Assert.AreEqual(comException.HResult, Constants.ErrorCode.ERROR_BLOCKED_BY_POLICY);
+            }
+        }
+    }
+}
