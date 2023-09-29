9699ed6b0c7ee78bab2aba5596fa7bf686e32de5
JohnMcPMS <johnmcp@microsoft.com>
2023-09-28 19:52:33 -0700

    Revert "Revert "Enable COM API access to correlate with the tracking database only (#3670)" (#3700)"
    This reverts commit 2cfe7865ee064a6710ae689308ae8c210fe105fd.

--- src/AppInstallerRepositoryCore/Public/winget/RepositorySource.h
+++ src/AppInstallerRepositoryCore/Public/winget/RepositorySource.h
@@ -225,6 +225,10 @@ namespace AppInstaller::Repository
         // Set background update check interval.
         void SetBackgroundUpdateInterval(TimeSpan interval);
 
+        // Indicates that we are only interested in the PackageTrackingCatalog for the source.
+        // Must be set before Open to have effect, and will prevent the underlying source from being updated or opened.
+        void InstalledPackageInformationOnly(bool value);
+
         // Execute a search on the source.
         SearchResult Search(const SearchRequest& request) const;
 
@@ -287,6 +291,7 @@ namespace AppInstaller::Repository
         bool m_isSourceToBeAdded = false;
         bool m_isComposite = false;
         std::optional<TimeSpan> m_backgroundUpdateInterval;
+        bool m_installedPackageInformationOnly = false;
         mutable PackageTrackingCatalog m_trackingCatalog;
     };
 }
