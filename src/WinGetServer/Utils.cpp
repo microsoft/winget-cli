07ef47e8a6676ab13cc2f8c7acc3a242260204cc
John McPherson <johnmcp@microsoft.com>
2024-03-20 12:14:50 -0700

    Better server start coordination

--- src/WinGetServer/Utils.cpp
+++ src/WinGetServer/Utils.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 #include "Utils.h"
 #pragma warning( push )
@@ -32,4 +32,24 @@ std::string GetUserSID()
     LPSTR pszSID = NULL;
     THROW_LAST_ERROR_IF(!ConvertSidToStringSidA(pTokenUser->User.Sid, &pszSID));
     return std::string{ pszSID };
+}
+
+wil::unique_event CreateOrOpenServerStartEvent()
+{
+    wil::unique_event result;
+
+    for (int i = 0; i < 4; ++i)
+    {
+        if (!result.try_create(wil::EventOptions::ManualReset, L"WinGetServerStartEvent"))
+        {
+            if (result.try_open(L"WinGetServerStartEvent"))
+            {
+                break;
+            }
+        }
+    }
+
+    THROW_LAST_ERROR_IF(!result);
+
+    return result;
 }
