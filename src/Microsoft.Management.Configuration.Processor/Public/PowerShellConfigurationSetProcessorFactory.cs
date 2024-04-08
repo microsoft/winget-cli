26a44864e9b6b07259921e814241d53d7d0dbabd
Ryan <69221034+ryfu-msft@users.noreply.github.com>
2024-04-08 11:31:38 -0700

    Add support for optional limitation set in configuration remote server and processors #4349 (#4360)
    Cherry pick for #4349 into 1.7 release branch

--- src/Microsoft.Management.Configuration.Processor/Public/PowerShellConfigurationSetProcessorFactory.cs
+++ src/Microsoft.Management.Configuration.Processor/Public/PowerShellConfigurationSetProcessorFactory.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // <copyright file="PowerShellConfigurationSetProcessorFactory.cs" company="Microsoft Corporation">
 //     Copyright (c) Microsoft Corporation. Licensed under the MIT License.
 // </copyright>
@@ -8,8 +8,11 @@ namespace Microsoft.Management.Configuration.Processor
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.IO;
+    using System.Linq;
     using System.Management.Automation;
+    using System.Runtime.CompilerServices;
     using System.Text;
     using Microsoft.Management.Configuration;
     using Microsoft.Management.Configuration.Processor.ProcessorEnvironments;
@@ -21,6 +24,17 @@ namespace Microsoft.Management.Configuration.Processor
     /// </summary>
     public sealed class PowerShellConfigurationSetProcessorFactory : IConfigurationSetProcessorFactory, IPowerShellConfigurationProcessorFactoryProperties
     {
+        private bool isCreateProcessorInvoked = false;
+
+        // Backing variables for properties that are restricted in limit mode.
+        private ConfigurationSet? limitationSet;
+        private PowerShellConfigurationProcessorType processorType = PowerShellConfigurationProcessorType.Default;
+        private IReadOnlyList<string>? additionalModulePaths;
+        private IReadOnlyList<string>? implicitModulePaths;
+        private PowerShellConfigurationProcessorPolicy policy = PowerShellConfigurationProcessorPolicy.Default;
+        private PowerShellConfigurationProcessorLocation location = PowerShellConfigurationProcessorLocation.Default;
+        private string? customLocation;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PowerShellConfigurationSetProcessorFactory"/> class.
         /// </summary>
@@ -39,39 +53,212 @@ namespace Microsoft.Management.Configuration.Processor
         public DiagnosticLevel MinimumLevel { get; set; } = DiagnosticLevel.Informational;
 
         /// <summary>
+        /// Gets or sets the limitation set. Limitation set can only be set once.
+        /// </summary>
+        public ConfigurationSet? LimitationSet
+        {
+            get
+            {
+                return this.limitationSet;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting LimitationSet in limit mode is invalid.");
+                }
+
+                this.limitationSet = value;
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the processor type.
         /// </summary>
-        public PowerShellConfigurationProcessorType ProcessorType { get; set; } = PowerShellConfigurationProcessorType.Default;
+        public PowerShellConfigurationProcessorType ProcessorType
+        {
+            get
+            {
+                return this.processorType;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting ProcessorType in limit mode is invalid.");
+                }
+
+                this.processorType = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the additional module paths.
         /// </summary>
-        public IReadOnlyList<string>? AdditionalModulePaths { get; set; }
+        public IReadOnlyList<string>? AdditionalModulePaths
+        {
+            get
+            {
+                return this.additionalModulePaths;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting AdditionalModulePaths in limit mode is invalid.");
+                }
+
+                // Create a copy of incoming value
+                List<string> newModulePaths = new List<string>();
+                if (value != null)
+                {
+                    foreach (string path in value)
+                    {
+                        newModulePaths.Add(path);
+                    }
+                }
+
+                // Add implicit module paths if applicable
+                if (this.implicitModulePaths != null)
+                {
+                    foreach (string path in this.implicitModulePaths)
+                    {
+                        if (!newModulePaths.Contains(path))
+                        {
+                            newModulePaths.Add(path);
+                        }
+                    }
+                }
+
+                this.additionalModulePaths = newModulePaths;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the implicit module paths. These paths are always included in AdditionalModulePaths.
+        /// </summary>
+        public IReadOnlyList<string>? ImplicitModulePaths
+        {
+            get
+            {
+                return this.implicitModulePaths;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting ImplicitModulePaths in limit mode is invalid.");
+                }
+
+                this.implicitModulePaths = value;
+
+                // Apply to additional module paths if applicable.
+                if (this.implicitModulePaths != null)
+                {
+                    List<string> newModulePaths = new List<string>();
+                    if (this.additionalModulePaths != null)
+                    {
+                        foreach (string path in this.additionalModulePaths)
+                        {
+                            newModulePaths.Add(path);
+                        }
+                    }
+
+                    foreach (string path in this.implicitModulePaths)
+                    {
+                        if (!newModulePaths.Contains(path))
+                        {
+                            newModulePaths.Add(path);
+                        }
+                    }
+
+                    this.additionalModulePaths = newModulePaths;
+                }
+            }
+        }
 
         /// <summary>
         /// Gets or sets the configuration policy.
         /// </summary>
-        public PowerShellConfigurationProcessorPolicy Policy { get; set; } = PowerShellConfigurationProcessorPolicy.Default;
+        public PowerShellConfigurationProcessorPolicy Policy
+        {
+            get
+            {
+                return this.policy;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting Policy in limit mode is invalid.");
+                }
+
+                this.policy = value;
+            }
+        }
 
         /// <summary>
-        /// Gets or sets the module scope.
+        /// Gets or sets the module location.
         /// </summary>
-        public PowerShellConfigurationProcessorLocation Location { get; set; } = PowerShellConfigurationProcessorLocation.Default;
+        public PowerShellConfigurationProcessorLocation Location
+        {
+            get
+            {
+                return this.location;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting Location in limit mode is invalid.");
+                }
+
+                this.location = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the install module path. Only used for Scope = Custom.
         /// </summary>
-        public string? CustomLocation { get; set; }
+        public string? CustomLocation
+        {
+            get
+            {
+                return this.customLocation;
+            }
+
+            set
+            {
+                if (this.IsLimitMode())
+                {
+                    throw new InvalidOperationException("Setting CustomLocation in limit mode is invalid.");
+                }
+
+                this.customLocation = value;
+            }
+        }
 
         /// <summary>
         /// Gets the configuration unit processor details for the given unit.
         /// </summary>
-        /// <param name="set">Configuration Set.</param>
+        /// <param name="incomingSet">Configuration Set.</param>
         /// <returns>Configuration set processor.</returns>
-        public IConfigurationSetProcessor CreateSetProcessor(ConfigurationSet? set)
+        public IConfigurationSetProcessor CreateSetProcessor(ConfigurationSet? incomingSet)
         {
             try
             {
+                this.OnDiagnostics(DiagnosticLevel.Informational, $"The set processor factory is running in limit mode: {this.IsLimitMode()}.");
+
+                this.CheckLimitMode();
+
+                ConfigurationSet? set = this.IsLimitMode() ? this.limitationSet : incomingSet;
+
                 this.OnDiagnostics(DiagnosticLevel.Verbose, $"Creating set processor for `{set?.Name ?? "<null>"}`...");
 
                 if (set != null && (set.Parameters.Count > 0 || set.Variables.Count > 0))
@@ -119,7 +306,7 @@ namespace Microsoft.Management.Configuration.Processor
 
                 this.OnDiagnostics(DiagnosticLevel.Verbose, "... done creating set processor.");
 
-                return new ConfigurationSetProcessor(processorEnvironment, set) { SetProcessorFactory = this };
+                return new ConfigurationSetProcessor(processorEnvironment, set, this.IsLimitMode()) { SetProcessorFactory = this };
             }
             catch (Exception ex)
             {
@@ -201,6 +388,30 @@ namespace Microsoft.Management.Configuration.Processor
                 Message = message,
             };
             diagnostics.Invoke(this, information);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        private void CheckLimitMode()
+        {
+            if (!this.IsLimitMode())
+            {
+                return;
+            }
+
+            if (this.isCreateProcessorInvoked)
+            {
+                this.OnDiagnostics(DiagnosticLevel.Error, "CreateSetProcessor is already invoked in limit mode.");
+                throw new InvalidOperationException("CreateSetProcessor is already invoked in limit mode.");
+            }
+            else
+            {
+                this.isCreateProcessorInvoked = true;
+            }
+        }
+
+        private bool IsLimitMode()
+        {
+            return this.limitationSet != null;
         }
     }
 }
