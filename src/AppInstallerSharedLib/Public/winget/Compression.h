caa45a426d9d7eeb1f18d227c09d04a75ba966a6
John McPherson <johnmcp@microsoft.com>
2024-04-15 11:23:22 -0700

    starting on compression

---
+++ src/AppInstallerSharedLib/Public/winget/Compression.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+#include <AppInstallerLogging.h>
+#include <winrt/Windows.Foundation.h>
+#include <memory>
+
+namespace AppInstaller::Compression
+{
+
+    struct Compressor
+    {
+
+    };
+
+    struct Decompressor
+    {
+
+    };
+}
