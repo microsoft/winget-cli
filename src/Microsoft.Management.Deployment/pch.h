43425fe97d237e03026fca4530dbc422ab445595
JohnMcPMS <johnmcp@microsoft.com>
2024-07-08 14:53:36 -0700

    Fix for attempted use of capability API before it was introduced (#4620)
    ## Change
    Check for the existence of
    `Windows::Security::Authorization::AppCapabilityAccess::AppCapability`,
    and if it is not present, simply force the caller to be at least medium
    integrity level.

--- src/Microsoft.Management.Deployment/pch.h
+++ src/Microsoft.Management.Deployment/pch.h
@@ -1,9 +1,10 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 #pragma once
 #include <unknwn.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Metadata.h>
 #include <winrt/Windows.Web.Http.h>
 
 #include <ostream>
