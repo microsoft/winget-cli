095b221473deb749b7d20e04d125644a049579d1
JohnMcPMS <johnmcp@microsoft.com>
2024-08-14 14:57:51 -0700

    Create public sample (#4734)
    ## Change
    Copy the current dev tool, COM calling application into a new location
    and update significantly with the goal of being a public sample of
    usage. Cleaned up the code and tried to include more error handling and
    commenting.
    
    Major changes to the UX:
    - Removed the dev switch as this is intended as a public sample
    - Added example search options selection
    - Add `Upgrade` button so that function could be called as well
    - Add `Uninstall` button for installed packages
    - Disconnected installed and active operation lists from catalog
    selection and package search
    
    Major change to DevX:
    - Move to using the OOP COM nuget package
    - This isn't ready for use yet, as the OOP activation dll isn't
    available in a public version yet and we are still working on the exact
    shape of that update.

---
+++ samples/WinGetUWPCaller/WinGetUWPCaller/App.h
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::WinGetUWPCaller::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
