5db74d7d6efad10fb1d733dba002866e888afef8
John McPherson <johnmcp@microsoft.com>
2024-10-07 16:37:48 -0700

    Add extended error codes to results without them

--- src/Microsoft.Management.Deployment/ConnectResult.h
+++ src/Microsoft.Management.Deployment/ConnectResult.h
@@ -10,16 +10,18 @@ namespace winrt::Microsoft::Management::Deployment::implementation
         ConnectResult() = default;
         
 #if !defined(INCLUDE_ONLY_INTERFACE_METHODS)
-        void Initialize(winrt::Microsoft::Management::Deployment::ConnectResultStatus status, winrt::Microsoft::Management::Deployment::PackageCatalog packageCatalog);
+        void Initialize(winrt::Microsoft::Management::Deployment::ConnectResultStatus status, winrt::Microsoft::Management::Deployment::PackageCatalog packageCatalog, winrt::hresult extendedErrorCode);
 #endif
 
         winrt::Microsoft::Management::Deployment::ConnectResultStatus Status();
         winrt::Microsoft::Management::Deployment::PackageCatalog PackageCatalog();
+        winrt::hresult ExtendedErrorCode();
 
 #if !defined(INCLUDE_ONLY_INTERFACE_METHODS)
     private:
         winrt::Microsoft::Management::Deployment::ConnectResultStatus m_status = winrt::Microsoft::Management::Deployment::ConnectResultStatus::Ok;
         winrt::Microsoft::Management::Deployment::PackageCatalog m_packageCatalog{ nullptr };
+        winrt::hresult m_extendedErrorCode = S_OK;
 #endif
     };
 }
