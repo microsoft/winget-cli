36a4f1e9f574c0803b2669b33472e55e7649aaf0
John McPherson <johnmcp@microsoft.com>
2024-10-16 09:13:55 -0700

    Change meaning of AddOrUpdate return bool

--- src/AppInstallerCLITests/SQLiteIndex.cpp
+++ src/AppInstallerCLITests/SQLiteIndex.cpp
@@ -3918,20 +3918,20 @@ TEST_CASE("SQLiteIndex_AddOrUpdateManifest", "[sqliteindex]")
     {
         SQLiteIndex index = SQLiteIndex::Open(tempFile, SQLiteStorageBase::OpenDisposition::ReadWrite);
 
-        // Update with no updates should return false
+        // Update should return false
         REQUIRE(!index.AddOrUpdateManifest(manifest, manifestPath));
 
         manifest.DefaultLocalization.Add<Localization::Description>("description2");
 
-        // Update with no indexed updates should return false
+        // Update should return false
         REQUIRE(!index.AddOrUpdateManifest(manifest, manifestPath));
 
-        // Update with indexed changes
+        // Update with indexed changes should still return false
         manifest.DefaultLocalization.Add<Localization::PackageName>("Test Name2");
         manifest.Moniker = "testmoniker2";
         manifest.DefaultLocalization.Add<Localization::Tags>({ "t1", "t2", "t3" });
         manifest.Installers[0].Commands = {};
 
-        REQUIRE(index.AddOrUpdateManifest(manifest, manifestPath));
+        REQUIRE(!index.AddOrUpdateManifest(manifest, manifestPath));
     }
 }
