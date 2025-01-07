f52416c44c8c878144b8b5a55067296bacad6508
John McPherson <johnmcp@microsoft.com>
2025-01-06 20:12:47 -0800

    Impl, needs tests

--- src/AppInstallerRepositoryCore/Rest/RestClient.h
+++ src/AppInstallerRepositoryCore/Rest/RestClient.h
@@ -28,7 +28,7 @@ namespace AppInstaller::Repository::Rest
         static std::optional<AppInstaller::Utility::Version> GetLatestCommonVersion(const std::vector<std::string>& serverSupportedVersions, const std::set<AppInstaller::Utility::Version>& wingetSupportedVersions);
 
         // Responsible for getting the source information contracts with minimal validation. Does not try to create a rest interface out of it.
-        static Schema::IRestClient::Information GetInformation(const std::string& restApi, std::optional<std::string> customHeader, std::string_view caller, const Http::HttpClientHelper& helper);
+        static Schema::IRestClient::Information GetInformation(const std::string& restApi, const std::optional<std::string>& customHeader, std::string_view caller, const Http::HttpClientHelper& helper);
 
         static std::unique_ptr<Schema::IRestClient> GetSupportedInterface(
             const std::string& restApi,
@@ -39,7 +39,13 @@ namespace AppInstaller::Repository::Rest
             const Http::HttpClientHelper& helper);
 
         // Creates the rest client. Full validation performed (just as opening the source)
-        static RestClient Create(const std::string& restApi, std::optional<std::string> customHeader, std::string_view caller, const Http::HttpClientHelper& helper, const Authentication::AuthenticationArguments& authArgs = {});
+        static RestClient Create(
+            const std::string& restApi,
+            const std::optional<std::string>& customHeader,
+            std::string_view caller,
+            const Http::HttpClientHelper& helper,
+            const Schema::IRestClient::Information& information,
+            const Authentication::AuthenticationArguments& authArgs = {});
     private:
         RestClient(std::unique_ptr<Schema::IRestClient> supportedInterface, std::string sourceIdentifier);
 
