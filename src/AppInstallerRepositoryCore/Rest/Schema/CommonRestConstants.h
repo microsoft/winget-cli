fef5a7fadd0636dcdc8e62b92d4c83f389f7aea8
yao-msft <50888816+yao-msft@users.noreply.github.com>
2025-02-12 13:28:46 -0800

    Add support for 1.10 manifest in rest source parsing and winget utils interop (#5197)
    Tests added.

--- src/AppInstallerRepositoryCore/Rest/Schema/CommonRestConstants.h
+++ src/AppInstallerRepositoryCore/Rest/Schema/CommonRestConstants.h
@@ -13,6 +13,7 @@ namespace AppInstaller::Repository::Rest::Schema
     const Utility::Version Version_1_6_0{ "1.6.0" };
     const Utility::Version Version_1_7_0{ "1.7.0" };
     const Utility::Version Version_1_9_0{ "1.9.0" };
+    const Utility::Version Version_1_10_0{ "1.10.0" };
 
     // General API response constants
     constexpr std::string_view Data = "Data"sv;
