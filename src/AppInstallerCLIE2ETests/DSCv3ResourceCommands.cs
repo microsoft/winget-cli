2b46b163551e7cf588a1376e5a6dac6798590e70
John McPherson <johnmcp@microsoft.com>
2025-04-17 15:26:41 -0700

    Test fixes

--- src/AppInstallerCLIE2ETests/DSCv3ResourceCommands.cs
+++ src/AppInstallerCLIE2ETests/DSCv3ResourceCommands.cs
@@ -21,7 +21,7 @@ namespace AppInstallerCLIE2ETests
     [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1011:Closing square brackets should be spaced correctly", Justification = "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3687 pending SC 1.2 release")]
     public class DSCv3ResourceCommands
     {
-        private const string DefaultPackageIdentifier = "AppInstallerTest.TestExeInstaller";
+        private const string DefaultPackageIdentifier = Constants.ExeInstallerPackageId;
         private const string DefaultPackageLowVersion = "1.0.0.0";
         private const string DefaultPackageMidVersion = "1.1.0.0";
         private const string DefaultPackageHighVersion = "2.0.0.0";
@@ -748,7 +748,7 @@ namespace AppInstallerCLIE2ETests
 
             public string InstallMode { get; set; }
 
-            public bool? AcceptAgreements { get; set; }
+            public bool? AcceptAgreements { get; set; } = true;
         }
     }
 }
