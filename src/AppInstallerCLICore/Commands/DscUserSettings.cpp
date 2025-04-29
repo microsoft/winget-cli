0123f2c2e2c5c157c06c97bc399ab6a3252e6eeb
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2025-04-29 10:24:52 -0700

    Added export and schema

--- src/AppInstallerCLICore/Commands/DscUserSettings.cpp
+++ src/AppInstallerCLICore/Commands/DscUserSettings.cpp
@@ -24,51 +24,9 @@ namespace AppInstaller::CLI
         WINGET_DSC_DEFINE_COMPOSABLE_PROPERTY_FLAGS(SettingsProperty, Json::Value, Settings, "Settings", DscComposablePropertyFlag::Required | DscComposablePropertyFlag::CopyToOutput, Resource::String::DscResourcePropertyDescriptionPackageId);
         WINGET_DSC_DEFINE_COMPOSABLE_PROPERTY_ENUM(ActionProperty, std::string, Action, "Action", Resource::String::DscResourcePropertyDescriptionPackageMatchOption, ({ ACTION_PARTIAL, ACTION_FULL }), ACTION_FULL);
 
-        using UserSettingsObject = DscComposableObject<StandardExistProperty, StandardInDesiredStateProperty, SettingsProperty, ActionProperty>;
+        using UserSettingsObject = DscComposableObject<StandardInDesiredStateProperty, SettingsProperty, ActionProperty>;
 
-        bool TryReadUserSettings(Execution::Context& context, Json::Value& root)
-        {
-            auto settingsPath = UserSettings::SettingsFilePath();
-            std::ifstream file(settingsPath, std::ios::binary);
-            if (file)
-            {
-                Json::CharReaderBuilder builder;
-                std::string errs;
-                if (Json::parseFromStream(builder, file, &root, &errs))
-                {
-                    return true;
-                }
-                else
-                {
-                    context.Reporter.Error() << "Failed to parse settings file: " << settingsPath << ". Error: " << errs;
-                }
-            }
-            else
-            {
-                context.Reporter.Error() << "Error opening settings file: " << settingsPath;
-            }
-
-            return false;
-        }
-
-        bool TryWriteUserSettings(Execution::Context& context, Json::Value& root)
-        {
-            auto settingsPath = UserSettings::SettingsFilePath();
-            std::ofstream file(settingsPath, std::ios::binary);
-            if (file)
-            {
-                Json::StreamWriterBuilder writer;
-                file << Json::writeString(writer, root);
-            }
-            else
-            {
-                context.Reporter.Error() << "Error opening settings file for writing: " << settingsPath;
-            }
-
-            return true;
-        }
-
-        void MergeJson(Json::Value& target, const Json::Value& overlay)
+        void MergeUserSettings(Json::Value& target, const Json::Value& overlay)
         {
             if (!overlay.isObject() || !target.isObject())
             {
@@ -83,7 +41,7 @@ namespace AppInstaller::CLI
                     Json::Value& targetValue = target[overlayKey];
                     if (targetValue.isObject() && overlayValue.isObject())
                     {
-                        MergeJson(targetValue, overlayValue);
+                        MergeUserSettings(targetValue, overlayValue);
                     }
                     else
                     {
@@ -95,6 +53,79 @@ namespace AppInstaller::CLI
                     target[overlayKey] = overlayValue;
                 }
             }
+        }
+
+        bool TryReadUserSettings(Json::Value& root)
+        {
+            auto settingsPath = UserSettings::SettingsFilePath();
+            std::ifstream file(settingsPath, std::ios::binary);
+            if (file)
+            {
+                Json::CharReaderBuilder builder;
+                std::string errs;
+                if (Json::parseFromStream(builder, file, &root, &errs))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool TryWriteUserSettings(Json::Value& root)
+        {
+            auto settingsPath = UserSettings::SettingsFilePath();
+            std::ofstream file(settingsPath, std::ios::binary);
+            if (file)
+            {
+                Json::StreamWriterBuilder writer;
+                file << Json::writeString(writer, root);
+            }
+
+            return true;
+        }
+
+        bool TryProcessUserSettings(const UserSettingsObject& input, UserSettingsObject& output)
+        {
+            // Input settings property is required
+            if (!input.Settings())
+            {
+                return false;
+            }
+
+            // Always return the full settings object
+            output.Action(ACTION_FULL);
+
+            // Full: overwrite the settings file
+            if(input.Action() == ACTION_FULL)
+            {
+                output.Settings(input.Settings());
+                return true;
+            }
+
+            // Partial: merge the settings
+            Json::Value userSettings;
+            if (TryReadUserSettings(userSettings))
+            {
+                MergeUserSettings(userSettings, *input.Settings());
+                output.Settings(userSettings);
+                return true;
+            }
+
+            return false;
+        }
+
+        bool TryExportUserSettings(UserSettingsObject& output)
+        {
+            Json::Value userSettings;
+            if (TryReadUserSettings(userSettings))
+            {
+                output.Action(ACTION_FULL);
+                output.Settings(userSettings);
+                return true;
+            }
+
+            return false;
         }
     }
 
@@ -122,12 +153,9 @@ namespace AppInstaller::CLI
 
     void DscUserSettings::ResourceFunctionGet(Execution::Context& context) const
     {
-        Json::Value root;
-        if (TryReadUserSettings(context, root))
+        UserSettingsObject output;
+        if (TryExportUserSettings(output))
         {
-            UserSettingsObject output;
-            output.Action(ACTION_FULL);
-            output.Settings(root);
             WriteJsonOutputLine(context, output.ToJson());
         }
     }
@@ -137,41 +165,10 @@ namespace AppInstaller::CLI
         if (auto json = GetJsonFromInput(context))
         {
             UserSettingsObject input(json);
-            if (input.Settings())
+            UserSettingsObject output;
+            if (TryProcessUserSettings(input, output) && TryWriteUserSettings(*output.Settings()))
             {
-                UserSettingsObject output;
-                output.Action(ACTION_FULL);
-
-                // Full: overwrite the settings file
-                // Partial: merge the settings file
-                if (input.Action() == ACTION_PARTIAL)
-                {
-                    Json::Value root;
-                    if (TryReadUserSettings(context, root))
-                    {
-                        MergeJson(root, *input.Settings());
-                        output.Settings(root);
-                    }
-                    else
-                    {
-                        context.Reporter.Error() << "Failed to read settings file.";
-                        return;
-                    }
-                }
-                else
-                {
-                    output.Settings(input.Settings());
-                }
-
-                // Update the settings file
-                if (TryWriteUserSettings(context, *output.Settings()))
-                {
-                    WriteJsonOutputLine(context, output.ToJson());
-                }
-                else
-                {
-                    context.Reporter.Error() << "Failed to write settings file.";
-                }
+                WriteJsonOutputLine(context, output.ToJson());
             }
         }
     }
@@ -180,19 +177,28 @@ namespace AppInstaller::CLI
     {
         if (auto json = GetJsonFromInput(context))
         {
-            // TODO
+            UserSettingsObject input(json);
+            UserSettingsObject outputProcess;
+            UserSettingsObject outputExport;
+            if (TryProcessUserSettings(input, outputProcess) && TryExportUserSettings(outputExport))
+            {
+                outputExport.InDesiredState(outputProcess.Settings() == outputExport.Settings());
+                WriteJsonOutputLine(context, outputExport.ToJson());
+            }
         }
     }
 
     void DscUserSettings::ResourceFunctionExport(Execution::Context& context) const
     {
-        auto json = GetJsonFromInput(context, false);
-        // TODO
+        UserSettingsObject output;
+        if (TryExportUserSettings(output))
+        {
+            WriteJsonOutputLine(context, output.ToJson());
+        }
     }
 
     void DscUserSettings::ResourceFunctionSchema(Execution::Context& context) const
     {
-        // TODO
-        context;
+        WriteJsonOutputLine(context, UserSettingsObject::Schema(ResourceType()));
     }
 }
