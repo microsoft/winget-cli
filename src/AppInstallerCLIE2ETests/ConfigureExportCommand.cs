c8f506dbba124d5ecfc4fe5f3bded28794b7aea2
yao-msft <50888816+yao-msft@users.noreply.github.com>
2025-05-02 13:33:12 -0700

    update pipeline

--- src/AppInstallerCLIE2ETests/ConfigureExportCommand.cs
+++ src/AppInstallerCLIE2ETests/ConfigureExportCommand.cs
@@ -29,6 +29,7 @@ namespace AppInstallerCLIE2ETests
         {
             TestCommon.SetupTestSource(false);
             WinGetSettingsHelper.ConfigureFeature("configureExport", true);
+            WinGetSettingsHelper.ConfigureFeature("dsc3", true);
             var installDir = TestCommon.GetRandomTestDir();
             TestCommon.RunAICLICommand("install", $"AppInstallerTest.TestPackageExport -v 1.0.0.0 --silent -l {installDir}");
             this.previousPathValue = System.Environment.GetEnvironmentVariable("PATH");
@@ -44,6 +45,7 @@ namespace AppInstallerCLIE2ETests
             TestCommon.RunAICLICommand("uninstall", "AppInstallerTest.TestPackageExport");
             TestCommon.TearDownTestSource();
             WinGetSettingsHelper.ConfigureFeature("configureExport", false);
+            WinGetSettingsHelper.ConfigureFeature("dsc3", false);
             if (!string.IsNullOrEmpty(this.previousPathValue))
             {
                 System.Environment.SetEnvironmentVariable("PATH", this.previousPathValue);
