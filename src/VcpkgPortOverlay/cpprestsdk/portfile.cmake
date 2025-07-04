f7fa3bfa41d25bb0c04764727a925e4491cbe400
Flor Chacón <14323496+florelis@users.noreply.github.com>
2025-07-04 00:39:07 -0700

    Use cpprestsdk v2.10.18 (#5567)
    We are seeing a crash coming from cpprestsdk, which happened after
    moving to use vcpkg dependencies and updating to v2.10.19. This reverts
    to the version we knew to be good, but it may be that the actual issue
    is not from cpprestsdk but from one of the patches from vcpkg.
    
    We consume cpprestsdk from a local port overlay that we use to be able
    to patch it with support for certificate pinning, so the main change is
    to the script that creates the port overlay. The script makes a copy of
    the port from the official registry; and this PR updates it so that it
    can fetch a version from an older commit and not just the latest.
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/microsoft/winget-cli/pull/5567)

--- src/VcpkgPortOverlay/cpprestsdk/portfile.cmake
+++ src/VcpkgPortOverlay/cpprestsdk/portfile.cmake
@@ -1,18 +1,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/cpprestsdk
-    REF 411a109150b270f23c8c97fa4ec9a0a4a98cdecf
-    SHA512 4f604763f05d53e50dec5deaba283fa4f82d5e7a94c7c8142bf422f4c0bc24bcef00666ddbdd820f64c14e552997d6657b6aca79a29e69db43799961b44b2a1a
+    REF 122d09549201da5383321d870bed45ecb9e168c5
+    SHA512 c9ded33d3c67880e2471e479a38b40a14a9ff45d241e928b6339eca697b06ad621846260eca47b6b1b8a2bc9ab7bf4fea8d3e8e795cd430d8839beb530e16dd7
     HEAD_REF master
     PATCHES 
         fix-find-openssl.patch
         fix_narrowing.patch
         fix-uwp.patch
-        fix-clang-dllimport.patch # workaround for https://github.com/microsoft/cpprestsdk/issues/1710
-        silence-stdext-checked-array-iterators-warning.patch
-        fix-asio-error.patch
         add-server-certificate-validation.patch
 )
+
+set(OPTIONS)
+if(NOT VCPKG_TARGET_IS_UWP)
+    SET(WEBSOCKETPP_PATH "${CURRENT_INSTALLED_DIR}/share/websocketpp")
+    list(APPEND OPTIONS
+        -DWEBSOCKETPP_CONFIG=${WEBSOCKETPP_PATH}
+        -DWEBSOCKETPP_CONFIG_VERSION=${WEBSOCKETPP_PATH})
+endif()
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -30,6 +35,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/Release"
     ${configure_opts}
     OPTIONS
+        ${OPTIONS}
         ${FEATURE_OPTIONS}
         -DBUILD_TESTS=OFF
         -DBUILD_SAMPLES=OFF
