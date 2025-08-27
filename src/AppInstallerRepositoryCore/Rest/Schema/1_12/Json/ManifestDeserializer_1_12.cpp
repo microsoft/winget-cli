0f615d0aef672e544748544d3ee888dd0a4147f4
David Bennett <dbenne@microsoft.com>
2025-08-27 15:30:12 -0700

    Remove unchanged deserializer implementation

--- src/AppInstallerRepositoryCore/Rest/Schema/1_12/Json/ManifestDeserializer_1_12.cpp
+++ src/AppInstallerRepositoryCore/Rest/Schema/1_12/Json/ManifestDeserializer_1_12.cpp
@@ -9,20 +9,6 @@ using namespace AppInstaller::Manifest;
 
 namespace AppInstaller::Repository::Rest::Schema::V1_12::Json
 {
-    std::optional<Manifest::ManifestInstaller> ManifestDeserializer::DeserializeInstaller(const web::json::value& installerJsonObject) const
-    {
-        auto result = V1_10::Json::ManifestDeserializer::DeserializeInstaller(installerJsonObject);
-
-        if (result)
-        {
-            auto& installer = result.value();
-
-            installer.AuthInfo = ParseAuthenticationInfo(installerJsonObject, ParseAuthenticationInfoType::Installer, GetManifestVersion());
-        }
-
-        return result;
-    }
-
     Manifest::InstallerTypeEnum ManifestDeserializer::ConvertToInstallerType(std::string_view in) const
     {
         std::string inStrLower = Utility::ToLower(in);
