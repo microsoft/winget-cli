5128d23b498bd82f4b0e859db4ff4fb9a972e0aa
Kaleb Luedtke <trenlymc@gmail.com>
2025-10-09 14:58:27 -0500

    Ensure 64 tags are loaded and parsed through manifest validation properly

--- src/AppInstallerCLITests/YamlManifest.cpp
+++ src/AppInstallerCLITests/YamlManifest.cpp
@@ -35,6 +35,14 @@ namespace
         return true;
     }
 
+    const MultiValue s_ManifestV1Tags{
+            "appxsdk", "msixsdk", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+            "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36",
+            "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48",
+            "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+            "61", "62", "63", "64" };
+
     void ValidateError(
         const ValidationError& error,
         ValidationError::Level level,
@@ -141,7 +149,8 @@ namespace
         REQUIRE(manifest.DefaultLocalization.Get<Localization::ShortDescription>() == "This is MSIX SDK");
         REQUIRE(manifest.DefaultLocalization.Get<Localization::Description>() == "The MSIX SDK project is an effort to enable developers");
         REQUIRE(manifest.Moniker == "msixsdk");
-        REQUIRE(manifest.DefaultLocalization.Get<Localization::Tags>() == MultiValue{ "appxsdk", "msixsdk" });
+        auto tags = manifest.DefaultLocalization.Get<Localization::Tags>();
+		REQUIRE(std::equal(tags.begin(), tags.end(), s_ManifestV1Tags.begin())); // Check that the tags in the manifest match the first n tags in s_ManifestV1Tags
 
         if (manifestVer >= ManifestVer{ s_ManifestVersionV1_1 })
         {
@@ -528,6 +537,7 @@ namespace
             REQUIRE(localization1.Get<Localization::CopyrightUrl>() == "https://www.microsoft.com/msixsdk/copyright/UK");
             REQUIRE(localization1.Get<Localization::ShortDescription>() == "This is MSIX SDK UK");
             REQUIRE(localization1.Get<Localization::Description>() == "The MSIX SDK project is an effort to enable developers UK");
+			tags = localization1.Get<Localization::Tags>();
             REQUIRE(localization1.Get<Localization::Tags>() == MultiValue{ "appxsdkUK", "msixsdkUK" });
 
             if (manifestVer >= ManifestVer{ s_ManifestVersionV1_1 })
