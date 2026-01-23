db48e475b51b6311b1a817099574bbf9e02b57e5
AmirMS <104940545+AmelBawa-msft@users.noreply.github.com>
2026-01-23 10:53:57 -0800

    Add command builder with escaped user input (#5982)
    <!-- To check a checkbox place an "x" between the brackets. e.g: [x] -->
    
    - [ ] I have signed the [Contributor License
    Agreement](https://cla.opensource.microsoft.com/microsoft/winget-pkgs).
    - [ ] I have updated the [Release Notes](../doc/ReleaseNotes.md).
    - [ ] This pull request is related to an issue.
    
    -----
    - 🧱 Add a command builder with escaped user input
    - ⚠️ Unfortunately, using `StartInfo.ArgumentList` is not available in
    net48, hence manually implementing the escape method.
    - 🧪 Added unit tests
    
    ###### Microsoft Reviewers: [Open in
    CodeFlow](https://microsoft.github.io/open-pr/?codeflow=https://github.com/microsoft/winget-cli/pull/5982)

--- src/PowerShell/Microsoft.WinGet.Client.Engine/Commands/CliCommand.cs
+++ src/PowerShell/Microsoft.WinGet.Client.Engine/Commands/CliCommand.cs
@@ -33,7 +33,7 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void EnableSetting(string name)
         {
             Utilities.VerifyAdmin();
-            _ = this.Run("settings", $"--enable \"{name}\"");
+            _ = this.Run(new WinGetCLICommandBuilder("settings").AppendOption("enable", name));
         }
 
         /// <summary>
@@ -43,7 +43,7 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void DisableSetting(string name)
         {
             Utilities.VerifyAdmin();
-            _ = this.Run("settings", $"--disable \"{name}\"");
+            _ = this.Run(new WinGetCLICommandBuilder("settings").AppendOption("disable", name));
         }
 
         /// <summary>
@@ -52,7 +52,7 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         /// <param name="asPlainText">Return as string.</param>
         public void GetSettings(bool asPlainText)
         {
-            var result = this.Run("settings", "export");
+            var result = this.Run(new WinGetCLICommandBuilder("settings").AppendSubCommand("export"));
 
             if (asPlainText)
             {
@@ -75,24 +75,27 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void AddSource(string name, string arg, string type, string trustLevel, bool isExplicit)
         {
             Utilities.VerifyAdmin();
-            string parameters = $"add --name \"{name}\" --arg \"{arg}\"";
+            var builder = new WinGetCLICommandBuilder("source")
+                .AppendSubCommand("add")
+                .AppendOption("name", name)
+                .AppendOption("arg", arg);
 
             if (!string.IsNullOrEmpty(type))
             {
-                parameters += $" --type \"{type}\"";
+                builder.AppendOption("type", type);
             }
 
             if (!string.IsNullOrEmpty(trustLevel))
             {
-                parameters += $" --trust-level \"{trustLevel}\"";
+                builder.AppendOption("trust-level", trustLevel);
             }
 
             if (isExplicit)
             {
-                parameters += " --explicit";
+                builder.AppendSwitch("explicit");
             }
 
-            _ = this.Run("source", parameters, 300000);
+            _ = this.Run(builder, 300000);
         }
 
         /// <summary>
@@ -102,7 +105,7 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void RemoveSource(string name)
         {
             Utilities.VerifyAdmin();
-            _ = this.Run("source", $"remove --name \"{name}\"");
+            _ = this.Run(new WinGetCLICommandBuilder("source").AppendSubCommand("remove").AppendOption("name", name));
         }
 
         /// <summary>
@@ -112,7 +115,7 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void ResetSourceByName(string name)
         {
             Utilities.VerifyAdmin();
-            _ = this.Run("source", $"reset --name \"{name}\" --force");
+            _ = this.Run(new WinGetCLICommandBuilder("source").AppendSubCommand("reset").AppendOption("name", name).AppendSwitch("force"));
         }
 
         /// <summary>
@@ -121,13 +124,13 @@ namespace Microsoft.WinGet.Client.Engine.Commands
         public void ResetAllSources()
         {
             Utilities.VerifyAdmin();
-            _ = this.Run("source", $"reset --force");
+            _ = this.Run(new WinGetCLICommandBuilder("source").AppendSubCommand("reset").AppendSwitch("force"));
         }
 
-        private WinGetCLICommandResult Run(string command, string parameters, int timeOut = 60000)
+        private WinGetCLICommandResult Run(WinGetCLICommandBuilder builder, int timeOut = 60000)
         {
             var wingetCliWrapper = new WingetCLIWrapper();
-            var result = wingetCliWrapper.RunCommand(this, command, parameters, timeOut);
+            var result = wingetCliWrapper.RunCommand(this, builder, timeOut);
             result.VerifyExitCode();
 
             return result;
