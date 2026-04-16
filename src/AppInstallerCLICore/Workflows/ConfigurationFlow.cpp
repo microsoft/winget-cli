d39e9cc2c92485fc08e0a91fe644de2337bdb169
Kaleb Luedtke <trenlymc@gmail.com>
2026-04-16 14:05:28 -0500

    Limit configuration output to 120 characters even if redirected * This should be acceptable as there is a message when the text is truncated

--- src/AppInstallerCLICore/Workflows/ConfigurationFlow.cpp
+++ src/AppInstallerCLICore/Workflows/ConfigurationFlow.cpp
@@ -19,7 +19,6 @@
 #include <winget/SelfManagement.h>
 #include <winget/PathTree.h>
 #include <winrt/Microsoft.Management.Configuration.h>
-#include <limits>
 
 using namespace AppInstaller::CLI::Execution;
 using namespace winrt::Microsoft::Management::Configuration;
@@ -357,7 +356,7 @@ namespace AppInstaller::CLI::Workflow
                     truncated = true;
                 }
 
-                if (Utility::LimitOutputLines(lines, GetConsoleWidth().value_or(std::numeric_limits<size_t>::max()), maxLines))
+                if (Utility::LimitOutputLines(lines, GetConsoleWidth().value_or(120), maxLines))
                 {
                     truncated = true;
                 }
@@ -768,7 +767,7 @@ namespace AppInstaller::CLI::Workflow
             if (messageData.ShowDescription && !description.empty())
             {
                 constexpr size_t maximumDescriptionLines = 3;
-                size_t consoleWidth = GetConsoleWidth().value_or(std::numeric_limits<size_t>::max());
+                size_t consoleWidth = GetConsoleWidth().value_or(120);
                 std::vector<std::string> lines = Utility::SplitIntoLines(description, maximumDescriptionLines + 1);
                 bool wasLimited = Utility::LimitOutputLines(lines, consoleWidth, maximumDescriptionLines);
 
