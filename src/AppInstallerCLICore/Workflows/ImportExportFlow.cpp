c9ff483249359b9935408db666f82fd58131b16b
Kaleb Luedtke <trenlymc@gmail.com>
2026-06-21 16:11:36 -0500

    Revert unintentional whitespace change

--- src/AppInstallerCLICore/Workflows/ImportExportFlow.cpp
+++ src/AppInstallerCLICore/Workflows/ImportExportFlow.cpp
@@ -40,8 +40,7 @@ namespace AppInstaller::CLI::Workflow
             return [&](const T& source)
             {
                 SourceDetails sourceDetails = GetSourceDetails(source);
-                return sourceDetails.Type == details.Type &&
-                    sourceDetails.Identifier == details.Identifier;
+                return sourceDetails.Type == details.Type && sourceDetails.Identifier == details.Identifier;
             };
         }
 
